c2ef4c9d6a114d2868e50bf052174e910512261e
YANG YANG <SixGod2019@outlook.com>
2023-07-20 21:49:42 +0800

    add terraform get

--- cedc-terraform-development/iam-step-functions/step-functions-role.tf
+++ cedc-terraform-development/iam-step-functions/step-functions-role.tf
@@ -1,5 +1,5 @@
 module "step_functions_iam_role" {
-  source          = "./cedc-terraform-generic-modules/modules/iam"
+  source          = "../../cedc-terraform-generic-modules/modules/iam"
   role_name       = "step_functions_execute_role"
   assume_role_policy = file("${path.module}/trust_policy.json")
   policy_names    = ["step_functions_invoke_lambda_policy"]
