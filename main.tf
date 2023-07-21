f2b09758a49e07bc46af72d42d4d6e069fd05a15
YANG YANG <SixGod2019@outlook.com>
2023-07-22 01:56:54 +0800

    test

--- main.tf
+++ main.tf
@@ -13,3 +13,7 @@ module "__cedc_terraform_development_iam_step_functions" {
 module "__cedc_terraform_development_YangYang_Fact_xxx_step_functions" {
   source = "./cedc-terraform-development/YangYang-Fact-xxx/step-functions"
 }
+
+module "__cedc_terraform_development_YangYang_Fact_test1_step_functions" {
+  source = "./cedc-terraform-development/YangYang-Fact-test1/step-functions"
+}
