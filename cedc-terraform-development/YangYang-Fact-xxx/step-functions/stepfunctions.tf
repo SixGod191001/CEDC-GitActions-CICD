9908fa7c9f9230ca126bed42053eccc3425a5e47
YANG YANG <SixGod2019@outlook.com>
2023-07-21 23:49:50 +0800

    test

--- cedc-terraform-development/YangYang-Fact-xxx/step-functions/stepfunctions.tf
+++ cedc-terraform-development/YangYang-Fact-xxx/step-functions/stepfunctions.tf
@@ -1,7 +1,7 @@
 module "state_machine" {
   source            = "../../../cedc-terraform-generic-modules/modules/step-functions"
   state_machine_name = "aws_sfn_state_machine"
-  role_arn          = module.step_functions_iam_role.iam_role_arn
+  role_arn          = module.__cedc_terraform_development_iam_step_functions.step_functions_iam_role.iam_role_arn
   definition        = file("${path.module}/state_machine_definition.json")
   tags              = { "project" = "CEDC" }
 }
