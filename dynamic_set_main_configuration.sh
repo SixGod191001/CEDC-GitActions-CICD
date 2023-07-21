52ff21c0398bde4bd4430d795b0184c59d855237
YANG YANG <SixGod2019@outlook.com>
2023-07-21 09:54:36 +0800

    更新配置文件

--- dynamic_set_main_configuration.sh
+++ dynamic_set_main_configuration.sh
@@ -1,14 +1,16 @@
-bash
 #!/bin/bash
 
 # 获取所有符合模糊匹配的子目录
 subdirectories=$(find ./cedc-terraform-development -type d -name '*' -printf '%P\n')
 
+# 提取最底层的子目录并添加绝对路径
+subdirectories=$(echo "$subdirectories" | awk -F/ '{print "$(pwd)/" $0}')
+
 # 生成 Terraform 配置文件
 cat << EOF > main.tf
 variable "subdirectories" {
   type    = list(string)
-  default = [$(echo $subdirectories | sed -e 's/ /, /g')]
+  default = [$(echo $subdirectories | sed -e "s/ /, /g")]
 }
 
 module "development" {
