be9797e6cc14579ff46cd75c4fc3cc5d25d17640
YANG YANG <SixGod2019@outlook.com>
2023-07-21 19:45:21 +0800

    测试跨模块引用

--- cedc-terraform-development/YangYang-Fact-xxx/step-functions/stepfunctions.tf
+++ cedc-terraform-development/YangYang-Fact-xxx/step-functions/stepfunctions.tf
@@ -1,7 +1,12 @@
+data "terraform_remote_state" "step_functions_iam_role" {
+  backend = "s3"
+
+}
+
 module "state_machine" {
   source            = "../../../cedc-terraform-generic-modules/modules/step-functions"
   state_machine_name = "aws_sfn_state_machine"
-  role_arn          = aws_iam_role.step_functions_execute_role.arn
+  role_arn          = data.terraform_remote_state.step_functions_iam_role.outputs.role_arn
   definition        = file("${path.module}/state_machine_definition.json")
   tags              = { "project" = "CEDC" }
 }
