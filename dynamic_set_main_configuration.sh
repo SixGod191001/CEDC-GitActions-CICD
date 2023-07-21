515f300be31d9aa2f38ec8bc71af7352609f1e09
YANG YANG <SixGod2019@outlook.com>
2023-07-21 09:36:46 +0800

    更新配置

---
+++ dynamic_set_main_configuration.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 获取所有符合模糊匹配的子目录
+subdirectories=$(find ./cedc-terraform-development -type d -name '*' -printf '%P\n')
+
+# 生成 Terraform 配置文件
+cat << EOF > main.tf
+variable "subdirectories" {
+  type    = list(string)
+  default = [$(echo $subdirectories | sed -e 's/ /, /g')]
+}
+
+module "development" {
+  source = "\${var.subdirectories[count.index]}"
+  count  = length(var.subdirectories)
+}
+EOF
--- main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  subdirectories = fileset(path.cwd, "./cedc-terraform-development/*/")
-}
-
-module "development" {
-  source = element(local.subdirectories, count.index)
-  count  = length(local.subdirectories)
-}
