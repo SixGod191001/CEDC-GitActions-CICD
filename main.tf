165dafdbad05ce50dbf2037b484f5b8295879fd5
YANG YANG <SixGod2019@outlook.com>
2023-07-21 09:14:45 +0800

    main.tf增加模糊匹配

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
-module "development" {
-  source = "./cedc-terraform-development/iam-step-functions"
+locals {
+  module_directories = fileset(path.cwd, "cedc-terraform-development/*/")
 }
 
-
-
-
+module "development" {
+  source = "./cedc-terraform-development"
+  for_each = local.module_directories
+  name = each.key
+}
