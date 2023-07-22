955ce8f273205c159cedfb15b7125816db1c0a00
YANG YANG <SixGod2019@outlook.com>
2023-07-22 16:04:12 +0800

    rename

--- cedc-terraform-development/YangYang-Fact-xxx/step-functions/stepfunctions.tf
+++
@@ -1,7 +0,0 @@
-module "state_machine" {
-  source            = "../../../cedc-terraform-generic-modules/modules/step-functions"
-  state_machine_name = "aws_sfn_state_machine"
-  role_name          = "step_functions_execute_role"
-  definition        = file("${path.module}/state_machine_definition.json")
-  tags              = { "project" = "CEDC" }
-}
