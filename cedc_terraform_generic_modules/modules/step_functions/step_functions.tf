bc052bc77f0fbc9e142f4e7c9edc263f8b056d5d
YANG YANG <SixGod2019@outlook.com>
2023-07-22 21:44:09 +0800

    test

--- cedc_terraform_generic_modules/modules/step_functions/step_functions.tf
+++ cedc_terraform_generic_modules/modules/step_functions/step_functions.tf
@@ -1,14 +1,10 @@
-#data "aws_iam_role" "state_machine_role" {
-#  name = var.role_name
-#}
+data "aws_iam_role" "state_machine_role" {
+  name = var.role_name
+}
 
 resource "aws_sfn_state_machine" "aws_sfn_state_machine" {
   name       = var.state_machine_name
-#  role_arn   = data.aws_iam_role.state_machine_role.arn
-  role_arn   = module.cedc_terraform_development_cedc_step_functions_iam_common.module.step_functions_iam_role.aws_iam_role.iam_role.arn
+  role_arn   = data.aws_iam_role.state_machine_role.arn
   definition = var.definition
   tags       = var.tags
-  depends_on = [
-    module.cedc_terraform_development_cedc_step_functions_iam_common.module.step_functions_iam_role.aws_iam_role.iam_role
-  ]
 }
--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ variable "subdirectories" {
 module "cedc_terraform_development_cedc_step_functions_iam_common" {
   source = "./cedc_terraform_development/cedc_step_functions_iam_common"
 }
-
  module "cedc_terraform_development_cedc_step_functions_example_step_functions" {
    source = "./cedc_terraform_development/cedc_step_functions_example/step_functions"
+   depends_on = [module.cedc_terraform_development_cedc_step_functions_iam_common]
+
  }
