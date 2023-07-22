f4c012d88069b50dd5f162c6b6edb61dac7e1e37
YANG YANG <SixGod2019@outlook.com>
2023-07-22 16:20:56 +0800

    重构名字

---
+++ cedc_terraform_development/cedc_step_functions_example/step_functions/cedc_step_functions_example.tf
@@ -0,0 +1,7 @@
+module "state_machine" {
+  source            = "../../../cedc_terraform_generic_modules/modules/step_functions"
+  state_machine_name = "aws_sfn_state_machine1"
+  role_name          = "step_functions_execute_role"
+  definition        = file("${path.module}/state_machine_definition.json")
+  tags              = { "project" = "CEDC" }
+}
