aab9dedca54a289bca2a442ae0167c5b2b0b91bf
YANG YANG <SixGod2019@outlook.com>
2023-07-22 17:20:37 +0800

    删除所有资源便于测试

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-variable "subdirectories" {
-  type    = list(string)
-  default = [
- "./cedc_terraform_development/cedc_step_functions_example/step_functions",
- "./cedc_terraform_development/cedc_step_functions_iam_common"
-  ]
-}
-
-module "cedc_terraform_development_cedc_step_functions_iam_common" {
-  source = "./cedc_terraform_development/cedc_step_functions_iam_common"
-}
+#variable "subdirectories" {
+#  type    = list(string)
+#  default = [
+# "./cedc_terraform_development/cedc_step_functions_example/step_functions",
+# "./cedc_terraform_development/cedc_step_functions_iam_common"
+#  ]
+#}
+#
+#module "cedc_terraform_development_cedc_step_functions_iam_common" {
+#  source = "./cedc_terraform_development/cedc_step_functions_iam_common"
+#}
 
 # module "cedc_terraform_development_cedc_step_functions_example_step_functions" {
 #   source = "./cedc_terraform_development/cedc_step_functions_example/step_functions"
