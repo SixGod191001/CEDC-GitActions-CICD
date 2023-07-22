58d1fa35151b953aa594f893928ae0788ce57bd3
YANG YANG <SixGod2019@outlook.com>
2023-07-22 16:20:26 +0800

    重构名字

--- dynamic_set_main_configuration.sh
+++ dynamic_set_main_configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取符合条件的所有 .tf 文件的父级目录路径
-directories=$(find ./cedc-terraform-development -type f -name '*.tf' -exec dirname {} \; | sort -u)
+directories=$(find ./cedc_terraform_development -type f -name '*.tf' -exec dirname {} \; | sort -u)
 
 # 生成 Terraform 配置文件
 cat << EOF > main.tf
