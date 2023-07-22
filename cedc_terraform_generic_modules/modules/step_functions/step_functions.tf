0b69b8708684beb3253e393602f828812a739cbe
YANG YANG <SixGod2019@outlook.com>
2023-07-22 16:55:03 +0800

    增加依赖

--- cedc_terraform_generic_modules/modules/step_functions/step_functions.tf
+++ cedc_terraform_generic_modules/modules/step_functions/step_functions.tf
@@ -2,9 +2,16 @@ data "aws_iam_role" "state_machine_role" {
   name = var.role_name
 }
 
+resource "null_resource" "dependency" {
+  depends_on = [data.aws_iam_role.state_machine_role]
+}
+
 resource "aws_sfn_state_machine" "aws_sfn_state_machine" {
   name        = var.state_machine_name
   role_arn    = data.aws_iam_role.state_machine_role.arn
   definition  = var.definition
   tags        = var.tags
+  depends_on = [
+    null_resource.dependency
+  ]
 }
