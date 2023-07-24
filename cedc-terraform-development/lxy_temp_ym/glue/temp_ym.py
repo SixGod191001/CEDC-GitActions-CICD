4a6ac5261c61572fa5ae964205bbe9edef22467b
linxinyu <131854300+SJQj@users.noreply.github.com>
2023-07-24 15:36:12 +0800

    Update temp_ym.py

--- cedc-terraform-development/lxy_temp_ym/glue/temp_ym.py
+++ cedc-terraform-development/lxy_temp_ym/glue/temp_ym.py
@@ -1,21 +1,59 @@
-import json
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
-from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
+from awsglue.job import Job
 
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
 
-def temp_ym():
-    # 创建 对象
-    sk = SparkSession.builder \
-        .appName("Read data from S3 to data processing") \
-        .getOrCreate()
+# 读取 s3 source 
+fact_ims_city_df = glueContext.create_dynamic_frame.from_options(
+    format_options={"withHeader": True, "separator": ","},
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://source-demo-test/demo-table-1.csv"],
+        "recurse": True,
+    }
+)
+fact_cpa_df = glueContext.create_dynamic_frame.from_options(
+    format_options={"withHeader": True, "separator": ","},
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://source-demo-test/demo-table-2.csv"],
+        "recurse": True,
+    }
+)
+# 筛选字段
 
-    # 读取 s3 source 文件
-    fact_ims_city_df = sk.read.csv("s3 source fact_ims_city path",sep=',',header=True)
-    fact_cpa_df = sk.read.csv("s3 source fact_ims_city path",sep=',',header=True)
-    # 找到列名为ym 并使用1作为join key
-    fact_ims_city_df_ym = fact_ims_city_df.selectExpr('ym','1 as joinkey').dropDuplicates()
-    fact_cpa_df_ym = fact_cpa_df.selectExpr('ym','1 as joinkey').dropDuplicates()
-    # 进行union操作
-    temp_ym_union = fact_cpa_df_ym.union(fact_ims_city_df_ym)
-    # 将 DataFrame 写入 s3 target temp_ym 文件
-    temp_ym_union.write.csv("s3 target temp_ym table path")
+# to DF()
+fact_ims_city_df = fact_ims_city_df.toDF()
+fact_cpa_df = fact_cpa_df.toDF()
+
+# 找到列名为ym 并使用1作为join key
+
+fact_ims_city_df_ym = fact_ims_city_df.selectExpr('ym','1 as joinkey').dropDuplicates()
+fact_cpa_df_ym = fact_cpa_df.selectExpr('ym','1 as joinkey').dropDuplicates()
+
+# 进行union操作
+
+temp_ym_union = fact_cpa_df_ym.union(fact_ims_city_df_ym)
+
+temp_ym_union_DY = DynamicFrame.fromDF(temp_ym_union, glueContext ,"temp_ym_union_DY")
+
+# 写入 s3 target 
+temp_ym = glueContext.write_dynamic_frame.from_options(
+    frame=temp_ym_union_DY,
+    connection_type="s3",
+    format="csv",
+    connection_options={"path": "s3://target-demo-test/temp_ym/"}
+)
+
+job.commit()
