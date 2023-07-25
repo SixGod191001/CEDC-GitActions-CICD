89d400c50e5d6baa1984d08d7e725548a53b0b31
ConnieGaoy <18642668618@163.com>
2023-07-25 13:10:16 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -1,20 +1,17 @@
+data "aws_iam_role" "cloudwatch_event_role" {
+  name = var.role_name
+}
+
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name                  = var.event_rule_name
   schedule_expression   =var.schedule_expression_details
   event_pattern         =var.event_pattern_details
   is_enabled            =var.event_enabled
+  role_arn   = data.aws_iam_role.cloudwatch_event_role.arn
 }
 
 resource "aws_cloudwatch_event_target" "event_rule_target" {
   rule                  = aws_cloudwatch_event_rule.event_rule.name
   depends_on            = [var.arn_details]
   arn                   =var.arn_details
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_trigger_event_rules" {
-  count                  = var.function_details_name == null ? 0 : 1
-  action                 = "lambda:InvokeFunction"
-  function_name          = var.function_details_name
-  principal              = "events.amazonaws.com"
-  source_arn             =  aws_cloudwatch_event_rule.event_rule.arn
 }
