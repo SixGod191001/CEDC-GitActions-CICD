416c50dd444f4738933f2a12da8aaed36aa4c249
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-07-26 18:01:09 +0800

    Create variables.tf
    Create Lambda

---
+++ cedc_terraform_generic_modules/modules/lambda/variables.tf
@@ -0,0 +1,24 @@
+variable "lambda_name" {
+  description = "The name of the lambda"
+  type = string
+}
+
+variable "lambda_role_arn" {
+  description = "The arn of the lambda role"
+  type = string
+}
+
+variable "lambda_handler" {
+  description = "The handler of the lambda"
+  type = string
+}
+
+variable "lambda_runtime" {
+  description = "The runtime of the lambda"
+  type = string
+}
+
+variable "source_code_hash" {
+  description = "The source code of the lambda"
+  type = string
+}
