7c84bf01dd68f494f6d1b40d91f922f123340e32
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-07-26 23:41:20 +0800

    Create variables.tf

---
+++ cedc_terraform_generic_modules/modules/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  description = "The name of the s3bucket"
+  type = string
+}
+
+variable "acl" {
+  description = "The acl of the s3bucket"
+  type = string
+}
+
+variable "tag" {
+  description = "The tag of the s3bucket"
+  type = string
+}
