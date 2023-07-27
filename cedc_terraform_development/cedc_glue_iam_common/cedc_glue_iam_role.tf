176c5a53a344a2bb6e443acdd6095dfa3d37400e
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-07-27 13:35:45 +0800

    Update cedc_glue_iam_role.tf

--- cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
+++ cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
@@ -1,6 +1,6 @@
 module "step_functions_iam_role" {
   source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "glue_execute_role"
+  role_name       = "glue_execute_role1"
   assume_role_policy = file("${path.module}/trust_policy.json")
   policy_names    = ["glue_invoke_s3_policy"]
   policy_contents = [file("${path.module}/glue_invoke_s3_policy.json")]
