053f6457e5fa3e91bf12858ae2ceae227c5a0862
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-07-27 12:04:48 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -1,6 +1,6 @@
 module "aws_lambda" {
   source        = "../../cedc-terraform-generic-modules/modules/lambda"
-  function_name = "cedc-lambda-trigger-sfn-xy"
+  function_name = "cedc-lambda-trigger-sfn-lyw"
   role          = lambda_to_stepfunctions_iam_role.arn
   handler       = "main.handler"
   runtime       = "python3.8"
