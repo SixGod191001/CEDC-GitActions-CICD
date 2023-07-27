1d140da6a64c3124039ae8a8a1a6f35841b152e6
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-07-27 11:32:22 +0800

    Create cedc_lambda_iam_role.tf

---
+++ cedc_terraform_development/cedc_terraform_development/cedc_lambda_iam_role.tf
@@ -0,0 +1,7 @@
+module "lambda_to_stepfunctions_iam_role" {
+  source          = "../../cedc_terraform_generic_modules/modules/iam"
+  role_name       = "lambda_to_stepfunctions_execute_role"
+  assume_role_policy = file("${path.module}/lambda_trust_policy.json")
+  policy_names    = ["lambda_to_stepfunctions_policy"]
+  policy_contents = [file("${path.module}/lambda_to_stepfunctions_policy.json")]
+}
