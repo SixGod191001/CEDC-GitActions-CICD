5c5fcb92d2dc7dfd0fba55c028667c5c173d4a7b
YANG YANG <SixGod2019@outlook.com>
2023-07-27 18:41:05 +0800

    yy

--- cedc_terraform_development/cicd_lambda_iam_common/cedc_lambda_iam_role.tf
+++ cedc_terraform_development/cicd_lambda_iam_common/cedc_lambda_iam_role.tf
@@ -1,7 +1,7 @@
-module "step_functions_iam_role" {
+module "lambda_iam_role" {
   source          = "../../cedc_terraform_generic_modules/modules/iam"
   role_name       = "lambda_execute_role_test3"
   assume_role_policy = file("${path.module}/trust_policy.json")
-  policy_names    = ["lambda_invoke_step_functions_policy.json"]
+  policy_names    = ["lambda_invoke_step_functions_policy"]
   policy_contents = [file("${path.module}/lambda_invoke_step_functions_policy.json")]
 }
