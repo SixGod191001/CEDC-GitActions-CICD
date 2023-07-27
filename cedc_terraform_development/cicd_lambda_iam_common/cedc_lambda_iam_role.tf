9b21f060e9cf9a15e8def15efba858fe4cca69a4
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-07-27 17:25:38 +0800

    Update cedc_lambda_iam_role.tf

--- cedc_terraform_development/cicd_lambda_iam_common/cedc_lambda_iam_role.tf
+++ cedc_terraform_development/cicd_lambda_iam_common/cedc_lambda_iam_role.tf
@@ -1,6 +1,6 @@
 module "step_functions_iam_role" {
   source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "lambda_execute_role_test1"
+  role_name       = "lambda_execute_role_test2"
   assume_role_policy = file("${path.module}/trust_policy.json")
   policy_names    = ["lambda_invoke_step_functions_policy.json"]
   policy_contents = [file("${path.module}/lambda_invoke_step_functions_policy.json")]
