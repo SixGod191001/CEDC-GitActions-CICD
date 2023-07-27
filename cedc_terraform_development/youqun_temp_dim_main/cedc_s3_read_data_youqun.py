cf0de3cbb145a75fb7916951b1b2eade8ccfaf4d
wangyouqun <530192791@QQ.com>
2023-07-27 10:50:29 +0800

    change file for test glue module

--- cedc_terraform_development/youqun_temp_dim_main/cedc_s3_read_data_youqun.py
+++ cedc_terraform_development/youqun_temp_dim_main/cedc_s3_read_data_youqun.py
@@ -1,1 +1,30 @@
-print('youqun test job created')
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# 获取命令行参数
+args = getResolvedOptions(sys.argv, ['cedc-s3-read-s3-data-glue-job-youqun'])
+
+# 创建 SparkContext 和 GlueContext
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# 初始化 Glue 作业
+job = Job(glueContext)
+job.init(args['cedc-s3-read-s3-data-glue-job-youqun'], args)
+
+# 定义源和目标数据目录
+source_bucket = 'cicddevraw'
+source_key = 'source/dim_brand.py'
+target_bucket = 'cedc-gitaction-cicd-luyan'
+target_key = 'cedc_temp_dim_main-youqun.py'
+
+# 读取源表数据
+source_table = spark.read.format("parquet").load(f"s3://{source_bucket}/{source_key}")
+
+# 将数据写入目标表
+source_table.write.format("parquet").mode("overwrite").save(f"s3://{target_bucket}/{target_key}")
+
+# 结束 Glue 作业
+job.commit()
