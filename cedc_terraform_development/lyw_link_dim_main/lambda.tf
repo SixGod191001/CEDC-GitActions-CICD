b552f66e0bdd7d24fe94e7a75cb76b4894d8c999
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-07-28 17:35:08 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -1,12 +1,8 @@
-module "lambda" {
+resource "aws_lambda_fucntion" "lambda_vic" {
+  name          ="lambda_vic"
   source        = "../../cedc_terraform_generic_modules/modules/lambda"
-  role          = lambda_to_stepfunctions_execute_role000
+  role_arn      = lambda_to_stepfunctions_execute_role000
   handler       = "main.handler"
   runtime       = "python3.9"
-  environment {
-    variables = jsondecode(file("${path.module}/lambda_definition.json"))["environment"]["variables"]
-  }
-  tags = jsondecode(file("${path.module}/lambda_definition.json"))["tags"]
- 
+  source_code   =filebase64sha256("lambda_definition,json")
 }
-  
