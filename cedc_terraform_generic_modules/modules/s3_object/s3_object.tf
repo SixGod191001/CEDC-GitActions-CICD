e234493c58c44c328d7d12fcc62694381f80fcc9
wangyouqun <530192791@QQ.com>
2023-07-28 13:01:29 +0800

    test4 glue role

--- cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
+++ cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
@@ -1,9 +1,11 @@
 data "aws_s3_bucket" "glue_scripts_bucket" {
   bucket = var.scripts_bucket_location
+  region = var.scripts_bucket_region
+  name = var.scripts_bucket_name
 }
 
 resource "aws_s3_bucket_object" "glue_script" {
-  bucket = data.aws_s3_bucket.glue_scripts_bucket.id
-  key    = var.scripts_name
+  bucket = data.aws_s3_bucket.glue_scripts_bucket.name
+  key    = data.aws_s3_bucket.glue_scripts_bucket.bucket
   source = var.scripts_path
 }
--- cedc_terraform_generic_modules/modules/s3_object/variables.tf
+++ cedc_terraform_generic_modules/modules/s3_object/variables.tf
@@ -3,10 +3,20 @@ variable "scripts_bucket_location" {
   type = string
 }
 
-variable "scripts_name" {
-  description = "your-script-name"
+variable "scripts_bucket_name" {
+  description = "your-script-bucket-name"
   type = string
 }
+
+variable "scripts_bucket_region" {
+  description = "The region of the script bucket"
+  type = string
+}
+
+#variable "scripts_name" {
+#  description = "your-script-name"
+#  type = string
+#}
 
 variable "scripts_path" {
   description = "path/to/your/script.py"
