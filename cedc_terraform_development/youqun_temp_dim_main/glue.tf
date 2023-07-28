4a10b5c0072118c10b6fcc987177de824c4496cc
wangyouqun <530192791@QQ.com>
2023-07-28 11:05:24 +0800

    test3 glue role

--- cedc_terraform_development/youqun_temp_dim_main/glue.tf
+++ cedc_terraform_development/youqun_temp_dim_main/glue.tf
@@ -1,6 +1,6 @@
 module "glue_script" {
   source       = "../../cedc_terraform_generic_modules/modules/s3_object"
-  scripts_bucket_location ="s3://cedc-gitaction-cicd-luyan"
+  scripts_bucket_location ="s3://gitaction-s3-terraform"
   scripts_name ="cedc_s3_read_data_youqun.py"
   scripts_path ="../../cedc_terraform_development/youqun_temp_dim_main/cedc_s3_read_data_youqun.py"
 }
@@ -8,7 +8,7 @@ module "glue_script" {
 module "glue_job" {
   source             = "../../cedc_terraform_generic_modules/modules/glue"
   job_name           = "cedc-s3-read-s3-data-glue-job-youqun"
-  role_name          = "glue_execute_role"
-  s_location         = "s3://cedc-gitaction-cicd-luyan"
+  role_name          = "glue_execute_role1"
+  s_location         = "s3://gitaction-s3-terraform"
   dependencies       = ["cedc_terraform_development/cedc_glue_iam_common"]
 }
