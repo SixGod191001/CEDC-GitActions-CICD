647c2f60ef6df107af5272f8d29fd0e69dd4ef57
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-07-31 13:52:07 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -2,7 +2,7 @@ module "aws_cloudwatch_event_rule" {
   source          = "../../cedc_terraform_generic_modules/modules/cloud_watch_event"
   name            = "cedc-eventbridge-trigger-lambda"
 
-  role_name        = "eventbridge_invoke_lambda_iam_role"
+  role_name        = "eventbridge_invoke_lambda_execute_role"
 
   event_rule_name = "schedule_rate"
   schedule_expression_details = "rate(1 hour)"  # 每一小时触发一次
