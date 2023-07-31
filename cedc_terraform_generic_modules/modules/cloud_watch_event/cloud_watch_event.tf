b713f9ce154405f5dff25d28be553b2be613f12a
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-07-31 15:54:09 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -5,7 +5,6 @@ data "aws_iam_role" "cloudwatch_event_role" {
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name                  = var.event_rule_name
   schedule_expression   =var.schedule_expression_details
-  event_pattern         =var.event_pattern_details
   is_enabled            =var.event_enabled
   role_arn   = data.aws_iam_role.cloudwatch_event_role.arn
 }
