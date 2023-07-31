936eef515cd3ff5eaba148197d81518ce41a1618
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-07-31 17:20:10 +0800

    Create variables.tf

---
+++ cedc_terraform_generic_modules/modules/s3_object/variables.tf
@@ -0,0 +1,14 @@
+variable "scripts_bucket_name" {
+  description = "your-script-bucket-name"
+  type = string
+}
+
+variable "scripts_bucket_location" {
+  description = "The location of the script bucket"
+  type = string
+}
+
+variable "scripts_path" {
+  description = "path to your script.py"
+  type = string
+}
