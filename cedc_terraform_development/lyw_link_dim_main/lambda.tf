c9bbadf087bd3e6e3e8c7f611d4e2d13830f3d80
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-07-31 12:39:54 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "lambdavic" {
-  lambda_name          = "lambdavic"
-  source               = "../../cedc_terraform_generic_modules/modules/lambda"
-  lambda_role_arn      = "lambda_to_stepfunctions_execute_role0001"
-  lambda_handler       = "main.handler"
-  lambda_runtime       = "python3.9"
+  role         = "lambdavic"
+  source       = "../../cedc_terraform_generic_modules/modules/lambda"
+  role_arn     = "lambda_to_stepfunctions_execute_role0001"
+  handler      = "main.handler"
+  runtime      = "python3.9"
  }
 
