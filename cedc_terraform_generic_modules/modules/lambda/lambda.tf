c95cabbd9298d3b9d96e18abb7d4f0bd12869e1e
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-07-31 13:04:23 +0800

    Update lambda.tf

--- cedc_terraform_generic_modules/modules/lambda/lambda.tf
+++ cedc_terraform_generic_modules/modules/lambda/lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda_name" {
-  name        = var.name
-  role_arn    = var.role_arn
+  name        = var.function_name
+  role_arn    = var.lambda_role_arn
   handler     = var.handler
   runtime     = var.runtime
   source_code = var.source_code_hash
