6ccc04d624379fa9266f0cf42fc7b95f654773ea
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-07-31 12:56:43 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -1,5 +1,5 @@
 module "lambdavic" {
-  role         = "lambdavic"
+  function_name   =  "lyw_labdavic"
   source       = "../../cedc_terraform_generic_modules/modules/lambda"
   role_arn     = "lambda_to_stepfunctions_execute_role0001"
   handler      = "main.handler"
