5a4e903c9cb1f482c03f6e168949da981c7098f0
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-07-31 16:18:48 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -4,7 +4,11 @@ module "lambdavic" {
   role_name = "lambda_to_stepfunctions_execute_role0001"
   handler       = "main.handler"
   runtime       = "python3.8"
-  s3_bucket     = "gitaction-s3-terraform"
-  s3_key        = "my-lambda-function.zip"
  }
 
+module "upload file" {
+  source       = "../../cedc_terraform_generic_modules/modules/s3_object"
+  bucket_name = "gitaction-s3-terraform"
+  bucket_location ="upload S3 file.py"
+  path ="${path.module}/upload S3 file.py"
+}
