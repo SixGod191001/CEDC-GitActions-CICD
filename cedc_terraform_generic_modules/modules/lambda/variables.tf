228a88c887ac3b5373c3dcca7597fb3ffd2723ff
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-07-31 14:30:08 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/lambda/variables.tf
+++ cedc_terraform_generic_modules/modules/lambda/variables.tf
@@ -3,11 +3,6 @@ variable "function_name" {
   type = string
 }
 
-variable "lambda_role_arn" {
-  description = "The arn of the lambda role"
-  type = string
-}
-
 variable "handler" {
   description = "The handler of the lambda"
   type = string
