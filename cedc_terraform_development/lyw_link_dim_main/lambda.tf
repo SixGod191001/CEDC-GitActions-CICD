ddebaf3c74cd4a0982ddf036c3b0eb380ab79d04
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-01 11:03:31 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -1,3 +1,11 @@
+module "lambda_script" {
+  source = "../../cedc_terraform_generic_modules/modules/s3_object"
+  scripts_bucket_name = "gitaction-s3-terraform"
+  scripts_name  = "upload_S3_file.py"
+  scripts_path = "${path.module}/upload_S3_file.py"
+  
+}
+
 module "lambdavic" {
   source       = "../../cedc_terraform_generic_modules/modules/lambda"
   function_name   =  "lyw_labdavic"
@@ -8,11 +16,3 @@ module "lambdavic" {
   s3_key        = "upload_S3_file.py"
   dependencies       = ["cedc_terraform_development/lyw_cedc_lambda_iam_common"]
  }
-
-module "lambda_script" {
-  source = "../../cedc_terraform_generic_modules/modules/s3_object"
-  scripts_bucket_name = "gitaction-s3-terraform"
-  scripts_name  = "upload_S3_file.py"
-  scripts_path = "${path.module}/upload_S3_file.py"
-  
-}
