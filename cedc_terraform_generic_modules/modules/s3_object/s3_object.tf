53caf9523f3a8e2ee26942e672ecd144ccf9e535
WangYouqun <131856644+WangYouqun@users.noreply.github.com>
2023-08-01 10:02:50 +0800

    Update s3_object.tf
    change var

--- cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
+++ cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "glue_script" {
   bucket = var.scripts_bucket_name
-  key    = var.scripts_bucket_location
+  key    = var.scripts_name
   source = var.scripts_path
 }
