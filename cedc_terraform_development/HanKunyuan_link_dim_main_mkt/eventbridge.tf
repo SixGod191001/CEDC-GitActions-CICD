fceab854b1a257cd8a68874c4102dfbcdaea862b
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-01 15:30:33 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -6,4 +6,3 @@ module "cloudwatch_event_rule" {
   arn_details                 ="arn:aws:lambda:ap-northeast-1:213903534337:function:Test_eb"
   dependencies                = ["cedc_terraform_development/cedc_eventbirdge"]
 }
-
