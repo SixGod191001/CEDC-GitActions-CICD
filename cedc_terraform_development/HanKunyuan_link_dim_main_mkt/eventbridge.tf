54c6867cf496149a971cd15a086247648107422b
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-01 16:45:59 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -3,6 +3,5 @@ module "cloudwatch_event_rule" {
   role_name                   = "eventbridge_invoke_lambda_execute_role"
   event_rule_name             ="cedc-eventbridge-trigger-lambda"
   schedule_expression_details = "rate(1 minute)"
-  arn_details                 ="arn:aws:lambda:ap-northeast-1:213903534337:function:Test_empty"
   dependencies                = ["cedc_terraform_development/cedc_eventbirdge"]
 }
