476b89f36892bedab001ebda2bbbd41da1190e10
luyan <luyanlovely@163.com>
2023-08-01 22:43:40 +0800

    test data ssm

--- cedc_terraform_development/cedc_s3_object_common/cedc_s3_object.tf
+++ cedc_terraform_development/cedc_s3_object_common/cedc_s3_object.tf
@@ -2,5 +2,5 @@ module "upload_s3_object" {
   source          = "../../cedc_terraform_generic_modules/modules/s3_object"
   scripts_bucket_name       = "scriptbucket"
   scripts_path ="${path.module}/test1.py"
-  scripts_bucket_location ="test1.py"
+  scripts_bucket_location ="glue-script/test2.py"
 }
