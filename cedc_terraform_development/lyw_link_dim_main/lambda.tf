6f2158bfa4685a38cf855b7512fd73c44a936f22
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-01 10:33:25 +0800

    Update lambda.tf

--- cedc_terraform_development/lyw_link_dim_main/lambda.tf
+++ cedc_terraform_development/lyw_link_dim_main/lambda.tf
@@ -4,9 +4,11 @@ module "lambdavic" {
   role_name = "lambda_to_stepfunctions_execute_role0002"
   handler       = "main.handler"
   runtime       = "python3.8"
+  s3_bucket     = "gitaction-s3-terraform"
+  s3_key        = "upload S3 file.py"
  }
 
-module "glue_script" {
+module "lambda_script" {
   source = "../../cedc_terraform_generic_modules/modules/s3_object"
   scripts_bucket_name = "gitaction-s3-terraform"
   scripts_name  = "upload S3 file.py"
