2c2f27442c50fbb6f5423aa8fe1725ab2be31603
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-02 17:09:28 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -1,11 +1,17 @@
-
+module "cloudwatch_event_rule" {
+  source                      = "../../cedc_terraform_generic_modules/modules/cloud_watch_event"
+  event_rule_name             = "cedc-eventbridge-trigger-lambda"
+  schedule_expression_details = "rate(1 minute)"
+  role_name                   = "eventbridge_invoke_lambda_execute_role"
+  dependencies                = ["cedc_terraform_development/cedc_eventbirdge"]
+}
 
 
 module "cloudwatch_event_rule_target" {
   source                      = "../../cedc_terraform_generic_modules/modules/cloud_watch_event_target"
-  event_rule_name             = "TEST"
+  event_rule_name             = "cedc-eventbridge-trigger-lambda"
   target_id                   = "Test"
   arn_details                 = "arn:aws:lambda:ap-northeast-1:213903534337:function:Test"
- # depends_on                  = [module.cloudwatch_event_rule]
+  depends_on                  = [module.cloudwatch_event_rule]
 }
 
