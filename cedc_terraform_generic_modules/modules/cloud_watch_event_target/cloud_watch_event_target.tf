a666b949662112aefc50ad871576e2be966bb581
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-03 17:16:01 +0800

    Create cloud_watch_event_target.tf

---
+++ cedc_terraform_generic_modules/modules/cloud_watch_event_target/cloud_watch_event_target.tf
@@ -0,0 +1,5 @@
+resource "aws_cloudwatch_event_target" "event_rule_target" {
+  rule         = var.event_rule_name  
+  target_id    = var.target_id
+  arn          = var.arn_details      # target arn
+}
