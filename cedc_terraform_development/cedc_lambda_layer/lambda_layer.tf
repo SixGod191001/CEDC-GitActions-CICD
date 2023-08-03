5379ae5b4e60cea8e20c33b72133125e91e75646
YANG YANG <SixGod2019@outlook.com>
2023-08-03 15:23:33 +0800

    test layer and update file path

--- cedc_terraform_development/cedc_lambda_layer/lambda_layer.tf
+++ cedc_terraform_development/cedc_lambda_layer/lambda_layer.tf
@@ -1,8 +1,8 @@
 module "lambda_layer" {
   source              = "../../cedc_terraform_generic_modules/modules/lambda_layer_version"
-  filename            = "utils.zip"
+  filename            = filebase64("${path.module}/utils.zip")
   layer_name          = "common"
   compatible_runtimes = ["python3.9"]
-  source_code_hash    = filebase64sha256("${path.module}/utils.zip")
+  source_code_hash    = filesha256("${path.module}/utils.zip")
 
 }
