893a3cbe3ed7bcdbe8ebcf981ac05d7f8eaf18e4
YANG YANG <SixGod2019@outlook.com>
2023-08-03 15:04:07 +0800

    filesha256

--- cedc_terraform_development/cedc_lambda_layer/lambda_layer.tf
+++ cedc_terraform_development/cedc_lambda_layer/lambda_layer.tf
@@ -1,6 +1,6 @@
 module "lambda_layer" {
   source          = "../../cedc_terraform_generic_modules/modules/lambda_layer_version"
-  filename   = filebase64("${path.module}/utils.zip")
+  filename   = filesha256("${path.module}/utils.zip")
   layer_name = "common"
   compatible_runtimes = ["python3.9"]
 
