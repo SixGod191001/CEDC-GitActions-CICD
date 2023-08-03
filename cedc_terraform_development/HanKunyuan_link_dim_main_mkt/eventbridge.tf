d10b815a0741e27b5fb5e0c327c19ad119a4e62f
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-03 16:40:49 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -6,15 +6,12 @@ module "cloudwatch_event_rule" {
   dependencies                = ["cedc_terraform_development/cedc_eventbirdge"]
 }
 
- output "eventbridge_name" {
-    value = cloudwatch_event_rule.event_rule_name.name
-}
 
 module "lambda_add_permission" {
   source                      = "../../cedc_terraform_generic_modules/modules/lambda_permissions"
   permission_statement_id     = "lambda_add_permission"
   lambda_function_name        = "Test"
-  execution_arn               = "arn:aws:events:ap-northeast-1:213903534337:rule/cedc-eventbridge-trigger-lambda"   #调用event bridge的arn
+  execution_arn               = "arn:aws:events:ap-northeast-1:213903534337:rule/cedc-eventbridge-trigger-lambda"   #eventbridge的arn
   depends_on                  = [module.cloudwatch_event_rule]
 }
 
@@ -23,8 +20,7 @@ module "cloudwatch_event_rule_target" {
   source                      = "../../cedc_terraform_generic_modules/modules/cloud_watch_event_target"
   event_rule_name             = "cedc-eventbridge-trigger-lambda"
   target_id                   = "Test"
-  # arn_details                 = "arn:aws:lambda:ap-northeast-1:213903534337:function:Test"
-  arn_details                 = module.cloudwatch_event_rule.eventbridge_name
+  arn_details                 = "arn:aws:lambda:ap-northeast-1:213903534337:function:Test"
   depends_on                  = [module.lambda_add_permission]
 }
 
