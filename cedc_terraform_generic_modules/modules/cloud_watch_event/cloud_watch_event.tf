1a1dae8f1780f81ba12bd3dc07869593f1d9f7ac
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-03 15:56:22 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -11,12 +11,10 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
 
  output "eventbridge_arn" {
     value = aws_cloudwatch_event_rule.event_rule.arn
-  }
 }
 
 /*
  output "eventbridge_name" {
     value = aws_cloudwatch_event_rule.event_rule.name
-  }
 }
 */
