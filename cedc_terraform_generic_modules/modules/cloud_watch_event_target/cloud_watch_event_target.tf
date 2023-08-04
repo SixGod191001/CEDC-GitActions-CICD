e80dba661b780acf538fa0d330b5b224c785eac9
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-04 15:05:17 +0800

    Update cloud_watch_event_target.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event_target/cloud_watch_event_target.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event_target/cloud_watch_event_target.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_target" "event_rule_target" {
-  rule         = aws_cloudwatch_event_rule.event_rule.name
+  rule         = var.event_rule_name
   target_id    = var.target_id
   arn          = var.arn_details
   dependencies =["cedc_terraform_generic_modules/modules/cloud_watch_event"]
