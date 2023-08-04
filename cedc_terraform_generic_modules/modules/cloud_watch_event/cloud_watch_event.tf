c8dede0ca7650dde92fb62e45b7240ebc2a983a2
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-04 17:24:32 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -4,7 +4,18 @@ data "aws_iam_role" "cloudwatch_event_role" {
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name                  = var.event_rule_name
-  schedule_expression   = var.schedule_expression_details     # 执行时间间隔
-  is_enabled            = var.event_enabled
-  role_arn              = data.aws_iam_role.cloudwatch_event_role.arn     # iam role
+  schedule_expression   = var.schedule_expression_details      # Trigger time (frequency/specific time)
+# event_pattern         = event_pattern_details                # Trigger condition
+  is_enabled            = var.event_enabled                    # Set the default value in the variable to "true"
+  role_arn              = data.aws_iam_role.cloudwatch_event_role.arn      
+}
+
+output "event_rule_arn" {
+  value       = aws_cloudwatch_event_rule.event_rule.arn
+  description = "output ARN of the created EventBridge rule"
+}
+
+ output "event_rule_name" {
+   value       = aws_cloudwatch_event_rule.event_rule.name
+   description = "output NAME of the created EventBridge rule"
 }
