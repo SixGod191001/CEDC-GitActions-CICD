e8789f711e3d74f070ec37090b817691276b1f1b
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-05 21:57:26 +0800

    Update glue.tf

--- cedc_terraform_development/youqun_temp_dim_main/glue.tf
+++ cedc_terraform_development/youqun_temp_dim_main/glue.tf
@@ -1,24 +1,30 @@
 module "glue_script" {
   source       = "../../cedc_terraform_generic_modules/modules/s3_object"
-  scripts_bucket_name = "gitaction-s3-terraform"
-  scripts_bucket_location ="cedc_s3_read_data_youqun.py"
-  scripts_path ="${path.module}/cedc_s3_read_data_youqun.py"
+  scripts_bucket_name = "scriptbucket"
+  scripts_name ="cicdgluescript.py"
+  scripts_path ="${path.module}/cicdgluescript.py"
 }
 
 module "glue_job" {
   source             = "../../cedc_terraform_generic_modules/modules/glue"
-  job_name           = "cedc-s3-read-s3-data-glue-job-youqun"
+  job_name           = "cicd-gitaction-glue-job"
   role_name          = "glue_execute_role"
-  s_location         = "s3://gitaction-s3-terraform"
-  dependencies        = ["cedc_terraform_development/cedc_glue_iam_common"]
-}
-
+  worker_number      =  2
+  work_type          = "Standard"
+  glue_version       = "2.0"
+  s3_path_header     = "s3://"
+  s3_path_tail       = "/cicdgluescript.py"
+  scripts_bucket_name = "scriptbucket"
+  dependencies       = ["cedc_terraform_development/cedc_glue_iam_common"]
+  depends_on         = [module.glue_script]
+  
+ }
 
 module "step_function_glue" {
   source                = "../../cedc_terraform_generic_modules/modules/step_functions"
-  state_machine_name    = "cedc-sfn-workflow-state-machine-mc"
+  state_machine_name    = "cicd-workflow-state-machine"
   role_name             = "step_functions_execute_role" 
-  definition            = file("${path.module}/state_machine_definition_mc.json")
+  definition            = file("${path.module}/state_machine_definition.json")
   tags                  = {}  # 可根据实际情况进行调整
   depends_on            = [module.glue_job]
 }
