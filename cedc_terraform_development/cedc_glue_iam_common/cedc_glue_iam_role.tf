dbc2e4a585957351671241529aeb9fc1c550e32f
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-06 18:29:29 +0800

    Update cedc_glue_iam_role.tf

--- cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
+++ cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
@@ -5,14 +5,3 @@ module "step_functions_iam_role" {
   policy_names    = ["glue_invoke_s3_policy"]
   policy_contents = [file("${path.module}/glue_invoke_s3_policy.json")]
 }
-
-resource "aws_iam_policy_attachment" "policy_attachment" {
-  name       = "my-policy-attachment"   # 示例：将 [Name for the policy attachment] 替换为 "my-policy-attachment"
-  roles      = [module.step_functions_iam_role.role_id]
-  policy_arn = "[arn:aws:iam::aws:policy/AmazonS3FullAccess]"
-}
-
-# 确保政策附加在 IAM 角色之前被创建和附加
-resource "aws_iam_policy_attachment" "policy_attachment" {
-  depends_on = [module.step_functions_iam_role]
-}
