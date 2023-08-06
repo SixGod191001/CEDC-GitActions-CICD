759135b38467e2c62e3474c122a0a8c94f9e06a3
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-06 12:21:19 +0800

    Update cedc_lambda_iam_role.tf

--- cedc_terraform_development/cedc_lambda_iam_common/cedc_lambda_iam_role.tf
+++ cedc_terraform_development/cedc_lambda_iam_common/cedc_lambda_iam_role.tf
@@ -1,6 +1,6 @@
 module "lambda_iam_role" {
   source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "lambda_execute_role"
+  role_name       = "lambda_execute_role00000"
   assume_role_policy = file("${path.module}/trust_policy.json")
   policy_names    = ["lambda_invoke_step_functions_policy.json"]
   policy_contents = [file("${path.module}/lambda_invoke_step_functions_policy.json")]
