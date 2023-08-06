7fc492ff3d68ea08aa4a2aab0d0f712dbbf76355
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-06 17:15:47 +0800

    Update lambda.tf

--- cedc_terraform_generic_modules/modules/lambda/lambda.tf
+++ cedc_terraform_generic_modules/modules/lambda/lambda.tf
@@ -11,6 +11,6 @@ resource "aws_lambda_function" "lambda_rule" {
   handler         = var.handler
   runtime         = var.runtime
   role            = data.aws_iam_role.lambda_function_role.arn
-  s3_bucket       = name.value
+  s3_bucket       = data.aws_ssm_parameter.ssm_param.value
   s3_key          = var.s3_key
 }
