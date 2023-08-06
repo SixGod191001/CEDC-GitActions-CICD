929022c04dfb750533a8b54274471b54d53320c5
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-06 17:10:44 +0800

    Update lambda.tf

--- cedc_terraform_generic_modules/modules/lambda/lambda.tf
+++ cedc_terraform_generic_modules/modules/lambda/lambda.tf
@@ -3,7 +3,7 @@ data "aws_iam_role" "lambda_function_role" {
 }
 
 data "aws_ssm_parameter" "ssm_param" {
- name          = var.scripts_bucket_name
+ name          = data.aws_ssm_parameter.ssm_param.value
 }
 
 resource "aws_lambda_function" "lambda_rule" {
@@ -11,6 +11,6 @@ resource "aws_lambda_function" "lambda_rule" {
   handler         = var.handler
   runtime         = var.runtime
   role            = data.aws_iam_role.lambda_function_role.arn
-  s3_bucket       = data.aws_ssm_parameter.ssm_param.value
+  s3_bucket       = name.value
   s3_key          = var.s3_key
 }
