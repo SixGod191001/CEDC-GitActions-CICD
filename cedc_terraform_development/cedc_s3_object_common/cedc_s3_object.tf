66670cbff68e691b38d074c8b2a567e0323a149a
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-06 10:25:59 +0800

    Delete cedc_terraform_development/cedc_s3_object_common directory

--- cedc_terraform_development/cedc_s3_object_common/cedc_s3_object.tf
+++
@@ -1,6 +0,0 @@
-module "upload_s3_object" {
-  source          = "../../cedc_terraform_generic_modules/modules/s3_object"
-  scripts_bucket_name       = "scriptbucket"
-  scripts_path ="${path.module}/test1.py"
-  scripts_bucket_location ="glue-script/test2.py"
-}
