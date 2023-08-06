ba7b5413764eb5e62b7ad4cf391924b6a6169584
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-06 18:50:33 +0800

    Update iam.tf

--- cedc_terraform_generic_modules/modules/iam/iam.tf
+++ cedc_terraform_generic_modules/modules/iam/iam.tf
@@ -11,9 +11,12 @@ resource "aws_iam_policy" "iam_policy" {
 
 resource "aws_iam_policy_attachment" "iam_policy_attachment" {
   name        = "attachment"
-  count       = length(var.policy_names)
-  policy_arn  = aws_iam_policy.iam_policy[count.index].arn
-  roles       = [aws_iam_role.iam_role.name]
+  policy_arn  = "[arn:aws:iam::aws:policy/AmazonS3FullAccess]"
+
+  roles = concat(
+    [aws_iam_role.iam_role.name],
+    [for i in aws_iam_policy.iam_policy : i.arn]
+  )
 }
 
 output "iam_role_arn" {
