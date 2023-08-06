6a45a4d007aa0b81e145bd86d51f0573e8ab5b46
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-06 18:18:05 +0800

    Update glue.tf

--- cedc_terraform_generic_modules/modules/glue/glue.tf
+++ cedc_terraform_generic_modules/modules/glue/glue.tf
@@ -1,22 +1,20 @@
 data "aws_iam_role" "glue_role" {
   name = var.role_name
 }
+
 data "aws_ssm_parameter" "ssm_param" {
  name  = var.scripts_bucket_name
 }
+
 resource "aws_glue_job" "glue_job" {
   name              = var.job_name
   role_arn          = data.aws_iam_role.glue_role.arn
   number_of_workers = var.worker_number
   worker_type       = var.work_type
   glue_version      = var.glue_version
+  s3_bucket         = data.aws_ssm_parameter.ssm_param.value
  
- 
-dynamic "command" {
-    for_each = [data.aws_ssm_parameter.ssm_param.value]
-    content {
-      script_location = "${var.s3_path_header}${command.value}${var.s3_path_tail}"
-    
-  }
+command {
+    script_location = "${var.s3_path_header}${s3_bucket}${var.s3_path_tail}"  
  }
 }
