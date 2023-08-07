0f9ed717acd201cff971eecb6939bb121d9e3531
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-07 17:49:09 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -2,12 +2,15 @@ data "aws_iam_role" "cloudwatch_event_role" {
   name = var.role_name
 }
 
-/*
-# Get the ssm parameters
-data "aws_ssm_parameter" "my_value" {
-  name = "myValue"
+# Get the ssm parameters_account
+data "aws_ssm_parameter" "Account" {
+  name = "ETL_Account"
 }
-*/
+
+# Get the ssm parameters_region
+data "aws_ssm_parameter" "Region" {
+  name = "ETL_Region"
+}
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name                  = var.event_rule_name
@@ -25,7 +28,7 @@ output "event_rule_arn" {
 */
 
 output "event_rule_arn_by_ssm" {
-  value       = format("arn:aws:events:%s:%s:rule/%s", "ap-northeast-1", "213903534337", var.event_rule_name)
+  value       = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
               # arn:aws:events:ap-northeast-1:213903534337:rule/cedc-eventbridge-trigger-lambda
   description = "output ARN of the created EventBridge rule by SSM"
 }
