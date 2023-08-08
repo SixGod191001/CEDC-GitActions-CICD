eb1b9048cc5dca73b66c912e5078d6bbd1b3b01a
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-08 17:34:38 +0800

    Update cloud_watch_event_target.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event_target/cloud_watch_event_target.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event_target/cloud_watch_event_target.tf
@@ -1,5 +1,9 @@
+data "aws_lambda_function" "lambda_function" { 
+  function_name    = var. lambda_function_name
+}
+
 resource "aws_cloudwatch_event_target" "event_rule_target" {
-  rule         = var.event_rule_name
-  target_id    = var.target_id
-  arn          = var.arn_details
+  rule                = var.event_rule_name
+  target_id           = var.target_id
+  target_arn          = data.aws_lambda_function.lambda_function.arn
 }
