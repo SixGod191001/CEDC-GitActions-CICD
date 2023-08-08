af38ae2c2a637845f10a5dde9d765aaaf2582586
Luyan <127569124+luyanAAAA@users.noreply.github.com>
2023-08-08 22:44:53 +0800

    Update lambda_permissions_var.tf

--- cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions_var.tf
+++ cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions_var.tf
@@ -1,16 +1,12 @@
+variable "event_rule_name" {
+    type = string
+}
+
+
 variable "permission_statement_id" {
     type = string
 }
 
 variable "lambda_function_name" {
    type = string
-}
-
-variable "execution_arn" {
-   type = string
-}
-
-variable "dependencies" {
-  type    = list(string)
-  default = []
 }
