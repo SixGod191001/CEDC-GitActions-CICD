8a0de0095894958d5a9c789e1be2a16506911751
Luyan <127569124+luyanAAAA@users.noreply.github.com>
2023-08-08 22:28:04 +0800

    Update eventbridge.tf

--- cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
+++ cedc_terraform_development/HanKunyuan_link_dim_main_mkt/eventbridge.tf
@@ -3,7 +3,7 @@ module "cloudwatch_event_rule" {
   source                      = "../../cedc_terraform_generic_modules/modules/cloud_watch_event"
   event_rule_name             = "cedc-eventbridge-trigger-lambda"
   schedule_expression_details = "rate(1 minute)"                                   # trigger every minute
-  role_name                   = "eventbridge_invoke_lambda_execute_role"
+  role_name                   = "eventbridge_invoke_lambda_execute_role1"
   dependencies                = ["cedc_terraform_development/cedc_eventbirdge"]    # depends on the created IAM
 }
 
