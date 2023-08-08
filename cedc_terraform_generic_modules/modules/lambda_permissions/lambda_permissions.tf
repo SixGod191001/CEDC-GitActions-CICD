fa43a3c5530d65164c957c1ab303722710c2991f
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-08 18:17:45 +0800

    Update lambda_permissions.tf

--- cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
+++ cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
@@ -1,11 +1,14 @@
+/*
 data "aws_cloudwatch_event_rule" "event_rule" {
   name = var.event_rule_name
 }
+*/
 
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id   = var.permission_statement_id
   action         = "lambda:InvokeFunction"
   function_name  = var.lambda_function_name
   principal      = "events.amazonaws.com"
-  source_arn     = data.aws_cloudwatch_event_rule.event_rule.arn
+# source_arn     = data.aws_cloudwatch_event_rule.event_rule.arn
+  source_arn     = var.execution_arn
 }
