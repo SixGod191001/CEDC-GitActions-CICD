f9142c72898e1727719e31a7edd231f83e9407d5
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-09 10:50:36 +0800

    Update lambda_permissions.tf

--- cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
+++ cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
@@ -1,9 +1,3 @@
-/*
-data "aws_cloudwatch_event_rule" "event_rule" {
-  name = var.event_rule_name
-}
-*/
-
 # Get the ssm parameters_account
 data "aws_ssm_parameter" "Account" {
   name = "ETL_Account"
@@ -14,8 +8,9 @@ data "aws_ssm_parameter" "Region" {
   name = "ETL_Region"
 }
 
+# Concatenate strings as arn and assign it to local variable
 locals {
-event_rule_arn = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
+  event_rule_arn = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
@@ -23,6 +18,5 @@ resource "aws_lambda_permission" "lambda_permission" {
   action         = "lambda:InvokeFunction"
   function_name  = var.lambda_function_name
   principal      = "events.amazonaws.com"
-# source_arn     = data.aws_cloudwatch_event_rule.event_rule.arn
   source_arn     = local.event_rule_arn
 }
