8171d21fe2a5a61bafd3634882672e910eabffad
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-09 11:29:01 +0800

    Update lambda_permissions.tf

--- cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
+++ cedc_terraform_generic_modules/modules/lambda_permissions/lambda_permissions.tf
@@ -8,8 +8,9 @@ data "aws_ssm_parameter" "Region" {
   name = "ETL_Region"
 }
 
+# Concatenate strings as arn and assign it to local variable
 locals {
-event_rule_arn = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
+  event_rule_arn = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
