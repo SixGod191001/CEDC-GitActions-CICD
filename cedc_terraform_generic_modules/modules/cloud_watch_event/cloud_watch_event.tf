13cdce1b40da24d8868053eecace37b0e46258ca
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-09 11:27:42 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -9,13 +9,3 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
   is_enabled            = var.event_enabled                    # Set the default value in the variable to "true"
   role_arn              = data.aws_iam_role.cloudwatch_event_role.arn      
 }
-
-output "event_rule_arn" {
-  value       = aws_cloudwatch_event_rule.event_rule.arn
-  description = "output ARN of the created EventBridge rule"
-}
-
- output "event_rule_name" {
-   value       = aws_cloudwatch_event_rule.event_rule.name
-   description = "output NAME of the created EventBridge rule"
-}
