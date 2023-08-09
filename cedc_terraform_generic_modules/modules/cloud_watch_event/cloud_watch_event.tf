c4ce2218d40914b3bd562cf1f55c1a8dcb9d099c
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-09 10:49:13 +0800

    Update cloud_watch_event.tf

--- cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
+++ cedc_terraform_generic_modules/modules/cloud_watch_event/cloud_watch_event.tf
@@ -2,16 +2,6 @@ data "aws_iam_role" "cloudwatch_event_role" {
   name = var.role_name
 }
 
-# Get the ssm parameters_account
-data "aws_ssm_parameter" "Account" {
-  name = "ETL_Account"
-}
-
-# Get the ssm parameters_region
-data "aws_ssm_parameter" "Region" {
-  name = "ETL_Region"
-}
-
 resource "aws_cloudwatch_event_rule" "event_rule" {
   name                  = var.event_rule_name
   schedule_expression   = var.schedule_expression_details      # Trigger time (frequency/specific time)
@@ -20,12 +10,9 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
   role_arn              = data.aws_iam_role.cloudwatch_event_role.arn      
 }
 
-output "event_rule_arn_by_ssm" {
-  value       = format("arn:aws:events:%s:%s:rule/%s", data.aws_ssm_parameter.Region.value, data.aws_ssm_parameter.Account.value, var.event_rule_name)
-  description = "output ARN of the created EventBridge rule by SSM"
-}
-
+/*
  output "event_rule_name" {
    value       = aws_cloudwatch_event_rule.event_rule.name
    description = "output NAME of the created EventBridge rule"
 }
+*/
