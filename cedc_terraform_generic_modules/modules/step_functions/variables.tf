d908c63b96fd4f16a19b7263acaaa18b941834a9
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-12 17:55:15 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/step_functions/variables.tf
+++ cedc_terraform_generic_modules/modules/step_functions/variables.tf
@@ -6,6 +6,7 @@ variable "state_machine_name" {
 variable "role_name" {
   description = "The name of the IAM role"
   type        = string
+  default     = "step_functions_execute_role"
 }
 
 variable "definition" {
