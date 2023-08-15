d1cf07e94e2880e7853d8e507f59721ebe038382
YANG YANG <SixGod2019@outlook.com>
2023-08-15 20:38:05 +0800

    test new s3 objects functions

--- cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
+++ cedc_terraform_generic_modules/modules/s3_object/s3_object.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "ssm_param" {
-  name     = var.s3_bucket_name
+  name = var.s3_bucket_name
 }
 
 data "local_file" "objects" {
@@ -7,8 +7,17 @@ data "local_file" "objects" {
   filename = element(var.files, count.index)
 }
 
+locals {
+  file_paths = flatten([
+    for file in data.local_file.objects : (
+      fileset(file, "**/*")  # 递归获取文件夹下的所有文件路径
+    )
+  ])
+}
+
 resource "aws_s3_object" "s3_objects" {
-  for_each = data.local_file.objects
+  for_each = { for idx, file in local.file_paths : idx => file }
+
   bucket   = data.aws_ssm_parameter.ssm_param.value
   key      = var.key
   source   = each.value
