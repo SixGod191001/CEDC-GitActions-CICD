e9521829acb5cb51574f2ade6d4ce2c6423663eb
YANG YANG <SixGod2019@outlook.com>
2023-08-17 20:34:21 +0800

    增加aws provider awscc

--- cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
+++ cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
@@ -1,6 +1,5 @@
-resource "aws_glue_databrew_dataset" "example_dataset" {
-  provider = awscc
-  name             = var.name
+resource "awscc_databrew_dataset" "example_dataset" {
+  name  = var.name
   input = {
     data_catalog_input_definition = {
       database_name    = var.database_name
