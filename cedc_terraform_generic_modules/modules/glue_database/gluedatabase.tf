0e86611ccfc01ae78e9efcd76a44796f1577c683
elephantjr <elephant.exe@qq.com>
2023-08-17 11:25:10 +0800

    Update gluedatabase.tf

--- cedc_terraform_generic_modules/modules/glue_database/gluedatabase.tf
+++ cedc_terraform_generic_modules/modules/glue_database/gluedatabase.tf
@@ -1,3 +1,9 @@
-resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
-  name = var.glue_database_name
+locals {
+  table_list = fileset(path.module, var.glue_catalog_table_list)
+} 
+
+resource "aws_glue_catalog_database" "create_catalog_database" {
+
+ for_each    = { for file in local.table_list : file => regex("[a-zA-Z\\-0-9_]+",file) }
+ name        = jsondecode(file("${path.module}/${each.key}")).database
 }
