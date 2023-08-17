e8f3c847acee23fa940f945a915ed1d61fde95df
elephantjr <elephant.exe@qq.com>
2023-08-17 11:41:12 +0800

    Update glue_catalog_table.tf

--- cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
+++ cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
@@ -1,22 +1,22 @@
 locals {
-  table_list        = fileset(path.module, var.glue_catalog_table_list)
+  table_list        = fileset(var.file_path, var.glue_catalog_table_list)
 } 
 
 resource "aws_glue_catalog_table" "create_catalog_table" {
 
   for_each          = { for file in local.table_list : file => regex("[a-zA-Z\\-0-9_]+",file) }
-  database_name     = jsondecode(file("${path.module}/${each.key}")).database                                                                       #aws_glue_catalog_database.create_catalog_database.name
-  name              = jsondecode(file("${path.module}/${each.key}")).table_name                                                                     #local.table_structure.table_name
+  database_name     = jsondecode(file("${var.file_path}/${each.key}")).database                                                                       #aws_glue_catalog_database.create_catalog_database.name
+  name              = jsondecode(file("${var.file_path}/${each.key}")).table_name                                                                     #local.table_structure.table_name
 
   #Storage Descriptor: Specify table's storage format and location
   storage_descriptor {
     input_format    = "org.apache.hadoop.mapred.TextInputFormat"
     output_format   = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
-    location        = "s3://${var.s3_bucket}cicddevraw/source/${jsondecode(file("${path.module}/${each.key}")).table_name}.csv"                                     #${each.value}/
+    location        = "s3://${var.file_bucket_name}/${var.s3_key}/${jsondecode(file("${var.file_path}/${each.key}")).table_name}.csv"
 
     dynamic "columns" {
       iterator      = columns
-      for_each      = jsondecode(file("${path.module}/${each.key}")).columns                                                                        #jsondecode(data.local_file.table_schema.content).columns               #local.table_structure.columns
+      for_each      = jsondecode(file("${var.file_path}/${each.key}")).columns                                                                        #jsondecode(data.local_file.table_schema.content).columns               #local.table_structure.columns
 
         content {
           name      = columns.value.name
