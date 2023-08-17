2a33c4512045b0ccc70f4311ddadfecb7a8d99ab
elephantjr <elephant.exe@qq.com>
2023-08-17 11:25:49 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/glue_database/variables.tf
+++ cedc_terraform_generic_modules/modules/glue_database/variables.tf
@@ -3,6 +3,11 @@ variable "glue_database_name" {
   type = string
 }
 
+variable "glue_catalog_table_list" {
+  description = "The glue catalog table list"
+  type = string
+}
+
 variable "dependencies" {
   type    = list(string)
   default = []
