0b8877c3f4fe9c18cd6a1ccd7513829c00be4aa5
YANG YANG <SixGod2019@outlook.com>
2023-08-17 20:27:25 +0800

    增加aws provider awscc

--- providers.tf
+++ providers.tf
@@ -1,7 +1,17 @@
 terraform {
+  required_providers {
+    awscc = {}
+    aws = {}
+  }
   backend "s3" {}
 }
 
 provider "aws" {
+  region = var.region
+}
+
+
+
+provider "awscc" {
   region = var.region
 }
