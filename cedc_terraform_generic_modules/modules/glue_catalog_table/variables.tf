341d209223d8849d53e06d1ac55d4ca8230dc7bd
elephantjr <elephant.exe@qq.com>
2023-08-17 11:30:13 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/glue_catalog_table/variables.tf
+++ cedc_terraform_generic_modules/modules/glue_catalog_table/variables.tf
@@ -3,12 +3,17 @@ variable "catalog_table_name" {
   type = string
 }
 
+variable "catalog_database_name" {
+  description = "The catalog database name"
+  type = string
+}
+
 variable "glue_catalog_table_list" {
   description = "The glue catalog table list"
   type = string
 }
 
-variable "catalog_database_name" {
-  description = "The catalog database name"
+variable "s3_bucket" {
+  description = "s3 bucket"
   type = string
 }
