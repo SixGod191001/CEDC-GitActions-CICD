062943b0484661b0f4f726d79f15454b5575aab7
elephantjr <elephant.exe@qq.com>
2023-08-17 13:12:38 +0800

    Update cedc_cicd_services.tf

--- cedc_terraform_development/youqun_temp_dim_main/cedc_cicd_services.tf
+++ cedc_terraform_development/youqun_temp_dim_main/cedc_cicd_services.tf
@@ -8,7 +8,7 @@ module "glue_catalog_table" {
   source                    = "../../cedc_terraform_generic_modules/modules/glue_catalog_table"
   table_strctr_file_path    = "${path.module}"
   glue_catalog_table_list   = "*_strctr.json"
-  data_file_bucket_name     = "cicddevraw"
+  datafile_bucket_name      = "cicddevraw"
   s3_key                    = "source"
 }
 
