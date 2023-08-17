8789c89a6d724147da32e2056c162951cbc5c998
elephantjr <elephant.exe@qq.com>
2023-08-17 11:44:31 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/glue_database/variables.tf
+++ cedc_terraform_generic_modules/modules/glue_database/variables.tf
@@ -3,6 +3,11 @@ variable "glue_database_name" {
   type = string
 }
 
+variable "table_strctr_file_path" {
+  description = "The glue catalog table strctr path"
+  type = string
+}
+
 variable "glue_catalog_table_list" {
   description = "The glue catalog table list"
   type = string
