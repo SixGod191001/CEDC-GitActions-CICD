fa9d5d99c6014f5620e1155a435bfafb8b74c447
YANG YANG <SixGod2019@outlook.com>
2023-08-17 20:41:38 +0800

    测试table 不存在情况

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,7 +1,7 @@
 module "prepared_s3_objects" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "test dataset"
-  database_name  = "testdb"
+  database_name  = "1"
   table_name     = "dim_brand_csv"
 }
 
