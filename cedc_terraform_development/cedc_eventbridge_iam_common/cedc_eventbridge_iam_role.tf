4cd5b29f4901d7d8203af3b2b9e7d6d86173a125
YANG YANG <SixGod2019@outlook.com>
2023-08-20 21:27:04 +0800

    add common role dev modules

---
+++ cedc_terraform_development/cedc_eventbridge_iam_common/cedc_eventbridge_iam_role.tf
@@ -0,0 +1,7 @@
+module "eventbridge_invoke_lambda_iam_role" {
+  source              = "../../cedc_terraform_generic_modules/modules/iam"
+  role_name           = "jacky_eventbridge_invoke_lambda_execute_role"
+  trust_policy  = file("${path.module}/eventbridge_trust_policy.json")
+  custom_policy_names        = ["jacky_invoke_lambda_policy"]
+  custom_policy_contents     = [file("${path.module}/eventbridge_invoke_lambda_policy.json")]
+}
