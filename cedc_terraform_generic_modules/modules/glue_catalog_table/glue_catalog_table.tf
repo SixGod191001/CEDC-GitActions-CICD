eb1ce3d4e4c7235e593cf929aed48d2a22cc3918
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-20 22:28:35 +0800

    Update glue_catalog_table.tf

--- cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
+++ cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
@@ -1,11 +1,35 @@
-resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
-  name          = var.catalog_table_name
-  database_name = var.catalog_database_name
-}
+locals {
+  table_list        = fileset(var.table_strctr_file_path, var.glue_catalog_table_list)
+} 
 
-  dynamic "columns" {
-        for_each = storage_descriptor.value.columns
+resource "aws_glue_catalog_table" "create_catalog_table" {
+
+  for_each          = { for file in local.table_list : file => regex("[a-zA-Z\\-0-9_]+",file) }
+  database_name     = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).database                                                                       #aws_glue_catalog_database.create_catalog_database.name
+  name              = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name                                                                     #local.table_structure.table_name
+
+  #Storage Descriptor: Specify table's storage format and location
+  storage_descriptor {
+    input_format    = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format   = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+    location        = "s3://${var.datafile_bucket_name}/${var.s3_key}/${jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name}.csv"
+
+    dynamic "columns" {
+      iterator      = columns
+      for_each      = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).columns                                                                        #jsondecode(data.local_file.table_schema.content).columns               #local.table_structure.columns
+
         content {
-          name = columns.value.name
-          type = columns.value.type
+          name      = columns.value.name
+          type      = columns.value.type
         }
+    }
+
+    ser_de_info {
+      name                  = "SerDe"
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ","
+      }
+    }
+  }
+}
