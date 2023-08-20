fe07ae0815e371c2753b0599f855fd0d5f6b0032
YANG YANG <SixGod2019@outlook.com>
2023-08-20 18:08:31 +0800

    gitaction无法拉取最新代码测试

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,9 +1,9 @@
-module "databrew_dataset" {
-  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "jacky-customers"
-  database_name  = "testdb"
-  table_name     = "customers"
-}
+#module "databrew_dataset" {
+#  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+#  name           = "jacky-customers"
+#  database_name  = "testdb"
+#  table_name     = "customers"
+#}
 
 
 
