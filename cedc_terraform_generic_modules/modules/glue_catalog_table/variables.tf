499a657be82a601f1793cbf3634babb7888c7401
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-20 22:29:13 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/glue_catalog_table/variables.tf
+++ cedc_terraform_generic_modules/modules/glue_catalog_table/variables.tf
@@ -1,5 +1,5 @@
 variable "table_strctr_file_path" {
-  description = "The glue catalog table strctr path"
+  description = "The glue catalog table list"
   type = string
 }
 
@@ -8,7 +8,12 @@ variable "glue_catalog_table_list" {
   type = string
 }
 
-variable "dependencies" {
-  type    = list(string)
-  default = []
+variable "datafile_bucket_name" {
+  description = "The burcket of the file"
+  type = string
+}
+
+variable "s3_key" {
+  description = "The s3 path tail"
+  type = string
 }
