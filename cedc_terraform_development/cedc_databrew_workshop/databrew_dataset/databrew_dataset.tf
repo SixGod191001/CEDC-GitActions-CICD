327262494dce12590e0d20ae829e32ea4168af0d
YANG YANG <SixGod2019@outlook.com>
2023-08-20 18:03:01 +0800

    修复dadaset名字问题

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,8 +1,10 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "jackycustomers"
+  name           = "jacky-customers"
   database_name  = "testdb"
   table_name     = "customers"
 }
+
+
 
 
