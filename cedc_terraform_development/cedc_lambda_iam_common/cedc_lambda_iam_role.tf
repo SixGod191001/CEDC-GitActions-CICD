368e4cbe88d911472c45bdce8ab1d3099916c0ff
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-21 11:34:02 +0800

    Delete cedc_terraform_development/cedc_lambda_iam_common directory

--- cedc_terraform_development/cedc_lambda_iam_common/cedc_lambda_iam_role.tf
+++
@@ -1,7 +0,0 @@
-module "lambda_iam_role" {
-  source                 = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name              = "jacky_lambda_execute_role"
-  trust_policy           = file("${path.module}/trust_policy.json")
-  custom_policy_names    = ["jacky_lambda_invoke_step_functions_policy"]
-  custom_policy_contents = [file("${path.module}/lambda_invoke_step_functions_policy.json")]
-}
