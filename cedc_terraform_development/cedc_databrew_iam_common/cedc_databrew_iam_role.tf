cf2aab8b5ee4eb816047bd9e23a443939f3de15b
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-21 20:48:05 +0800

    Update cedc_databrew_iam_role.tf

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,7 +1,7 @@
 module "cedc_databrew_common_role" {
   source                  = "../../cedc_terraform_generic_modules/modules/iam"
   use_service_role_arn    = true
-  role_name               = "macui_databrew_common_role1"
+  role_name               = "macui_databrew_common_role"
   trust_policy            = file("${path.module}/trust_policy.json")
   system_policy_names     = ["AWSGlueDataBrewServiceRole"]
   custom_policy_names     = ["macui_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "macui_AwsGlueDataBrewServicePolicyForInputS3-ID"]
