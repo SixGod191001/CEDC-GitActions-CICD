4cb1bad3b3297e84bb5b6b12261c29b47c6b8947
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-21 15:09:36 +0800

    Update cedc_glue_iam_role.tf

--- cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
+++ cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
@@ -6,6 +6,6 @@ module "step_functions_iam_role" {
   custom_policy_names    = ["macui_glue_invoke_s3_policy","macui_s3_full_policy","macui_glue_catalog_access","macui_glue_service_policy"]
   custom_policy_contents = [file("${path.module}/glue_invoke_s3_policy.json"),
                     file("${path.module}/s3_full_policy.json"),
-                    file("${path.module}/s3_full_policy.json"),
+                    file("${path.module}/glue_catalog_access.json"),
                     file("${path.module}/glue_service_policy.json")]
 }
