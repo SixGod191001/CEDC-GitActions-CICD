b181794be0c3c9474c95cef172e5dfba1be4fe55
YANG YANG <SixGod2019@outlook.com>
2023-08-21 19:20:23 +0800

    profile job增加逻辑动态判断是否需要使用data函数

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -4,6 +4,9 @@ module "databrew_profile_job" {
   role_name    = "jacky_databrew_common_role"
   type         = "PROFILE"
   mode         = "FULL_DATASET"
+  bucket       = "scriptbucket"
+  bucket_owner = "ETL_Account"
+  key          = "jacky_databrew_workshop/profile-output/"
   dependencies = [
     "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
   ]
