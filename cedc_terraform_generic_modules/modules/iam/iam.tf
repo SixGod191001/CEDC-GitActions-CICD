5e260ffc04835679053e0ab0b0a72dee1a52c079
YANG YANG <SixGod2019@outlook.com>
2023-08-21 18:50:01 +0800

    test 系统policy

--- cedc_terraform_generic_modules/modules/iam/iam.tf
+++ cedc_terraform_generic_modules/modules/iam/iam.tf
@@ -17,15 +17,15 @@ resource "aws_iam_policy_attachment" "iam_custom_policy_attachment" {
 }
 
 resource "aws_iam_policy_attachment" "iam_system_policy_attachment" {
-  name       = "system-policy-attachment-${var.system_policy_names[count.index]}"
-  count      = length(var.system_policy_names)
+  name       = "system-policy-attachment-${each.key}"
+  for_each   = toset(var.system_policy_names)
   roles      = [aws_iam_role.iam_role.name]
-  policy_arn = data.aws_iam_policy.system_policy.arn
+  policy_arn = data.aws_iam_policy.system_policy[each.key].arn
 }
 
 data "aws_iam_policy" "system_policy" {
-  count = length(var.system_policy_names)
-  name = var.system_policy_names[count.index]
+  for_each = toset(var.system_policy_names)
+  name     = each.key
 }
 
 
