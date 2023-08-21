773e07158d251ed5472b8899b288953f1a393395
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-21 16:46:08 +0800

    Update cedc_databrew_iam_role.tf

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,11 +1,12 @@
 module "cedc_databrew_common_role" {
-  source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "macui_databrew_common_role"
-  trust_policy = file("${path.module}/trust_policy.json")
-  system_policy_names = ["AWSGlueDataBrewServiceRole"]
-  custom_policy_names    = ["macui_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "macui_AwsGlueDataBrewServicePolicyForInputS3-ID"]
-  custom_policy_contents = [file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json"),file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json")]
-  use_service_role_arn   = true
+  source                  = "../../cedc_terraform_generic_modules/modules/iam"
+  use_service_role_arn    = true
+  role_name               = "macui_databrew_common_role"
+  trust_policy            = file("${path.module}/trust_policy.json")
+  system_policy_names     = ["AWSGlueDataBrewServiceRole"]
+  custom_policy_names     = ["macui_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "macui_AwsGlueDataBrewServicePolicyForInputS3-ID"]
+  custom_policy_contents  = [file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json"),file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json")]
+  
 }
 
 
