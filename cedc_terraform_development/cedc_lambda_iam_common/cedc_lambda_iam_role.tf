cd67e1bc3a8e9b35d8548b832551ba2d3355bcdd
liuzhu <62790503+Untitled1988@users.noreply.github.com>
2023-08-22 06:29:10 +0000

    create liuzhu role

--- cedc_terraform_development/cedc_lambda_iam_common/cedc_lambda_iam_role.tf
+++ cedc_terraform_development/cedc_lambda_iam_common/cedc_lambda_iam_role.tf
@@ -1,7 +1,7 @@
 module "lambda_iam_role" {
   source                 = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name              = "jacky_lambda_execute_role"
+  role_name              = "liuzhu_lambda_execute_role"
   trust_policy           = file("${path.module}/trust_policy.json")
-  custom_policy_names    = ["jacky_lambda_invoke_step_functions_policy"]
+  custom_policy_names    = ["liuzhu_lambda_invoke_step_functions_policy"]
   custom_policy_contents = [file("${path.module}/lambda_invoke_step_functions_policy.json")]
 }
