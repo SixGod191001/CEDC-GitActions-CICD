69838d6e51455b2a5bee89a53d5923081b278c1a
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-22 22:25:55 +0800

    Update glue_catalog_table.tf

--- cedc_terraform_development/cedc_centralize_code_demo/glue_catalog_table.tf
+++ cedc_terraform_development/cedc_centralize_code_demo/glue_catalog_table.tf
@@ -2,7 +2,7 @@ module "create_glue_catalog_table" {
   source                    = "../../cedc_terraform_generic_modules/modules/glue_catalog_table"
   table_strctr_file_path    = "${path.module}"
   glue_catalog_table_list   = "*_strctr.json"
-  datafile_bucket_name      = "cicddevraw"
-  s3_key                    = "source"
+  datafile_bucket_name      = "gitaction-s3-terraform"
+  #s3_key                    = "source"
   #depends_on                = [module.create_glue_database]
 }
