b2628142c4012e194f0453b837e0dd54230d3443
liuzhu <62790503+Untitled1988@users.noreply.github.com>
2023-08-22 06:34:14 +0000

    liuzhu dataset

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,6 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "jacky-customers"
+  name           = "liuzhu-customers"
   database_name  = "testdb"
   table_name     = "customers"
 }
