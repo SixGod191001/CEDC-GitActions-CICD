4e58482a05e3017ab9738e3a466764c8dc8a0913
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-22 12:12:01 +0800

    Update dataset.tf

--- cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
+++ cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
@@ -7,3 +7,21 @@ resource "awscc_databrew_dataset" "dataset" {
     }
   }
 }
+
+
+resource "awscc_databrew_datasets3" "datasets3" {
+  name = var.name
+  input = {
+    s3_input_definition = {
+      bucket = var.bucket
+      key    = var.key
+    }
+  }
+
+  format_options = {
+    csv = {
+      delimiter   = var.delimiter
+      header_row  = var.header_row
+    }
+  }
+}
