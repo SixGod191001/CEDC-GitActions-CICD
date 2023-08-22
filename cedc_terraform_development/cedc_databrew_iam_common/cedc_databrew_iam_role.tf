637a4ff3a04ea269c82e3d784955fbb6a63da0c1
liuzhu <62790503+Untitled1988@users.noreply.github.com>
2023-08-22 07:17:24 +0000

    liuzhu role

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,14 +1,10 @@
 module "cedc_databrew_common_role" {
-  source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "liuzhu_databrew_common_role"
-  trust_policy = file("${path.module}/trust_policy.json")
-  system_policy_names = ["AWSGlueDataBrewServiceRole"]
-  custom_policy_names    = ["liuzhu_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "liuzhu_AwsGlueDataBrewServicePolicyForInputS3-ID"]
-  custom_policy_contents = [file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json"),file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json")]
+  source                  = "../../cedc_terraform_generic_modules/modules/iam"
+  use_service_role_arn    = true
+  role_name               = "liuzhu_databrew_common_role"
+  trust_policy            = file("${path.module}/trust_policy.json")
+  system_policy_names     = ["AWSGlueDataBrewServiceRole"]
+  custom_policy_names     = ["liuzhu_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "liuzhu_AwsGlueDataBrewServicePolicyForInputS3-ID"]
+  custom_policy_contents  = [file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json"),file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json")]
+  
 }
-
-
-
-
-
-
