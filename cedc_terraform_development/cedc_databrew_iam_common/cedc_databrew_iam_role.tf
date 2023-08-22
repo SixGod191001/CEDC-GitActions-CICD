2f2b3a3e67878f01347f180ad7f2d9264c635e47
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-22 10:47:16 +0800

    Update cedc_databrew_iam_role.tf

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,9 +1,9 @@
 module "cedc_databrew_common_role" {
   source          = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name       = "jacky_databrew_common_role"
+  role_name       = "haohao_databrew_common_role"
   trust_policy = file("${path.module}/trust_policy.json")
   system_policy_names = ["AWSGlueDataBrewServiceRole"]
-  custom_policy_names    = ["jacky_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "jacky_AwsGlueDataBrewServicePolicyForInputS3-ID"]
+  custom_policy_names    = ["haohao_AwsGlueDataBrewServicePolicyForOutputS3-ID" , "haohao_AwsGlueDataBrewServicePolicyForInputS3-ID"]
   custom_policy_contents = [file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json"),file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3-ID.json")]
 }
 
