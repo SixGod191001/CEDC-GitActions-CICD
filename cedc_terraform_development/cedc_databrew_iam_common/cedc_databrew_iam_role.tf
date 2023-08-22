1274b54802b72047d3e58b73be9022df27c39d84
YANG YANG <SixGod2019@outlook.com>
2023-08-22 08:26:14 +0800

    add read policy for glue catalog

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,16 +1,19 @@
 module "cedc_databrew_common_role" {
-  source                 = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name              = "jacky_databrew_common_role"
-  trust_policy           = file("${path.module}/trust_policy.json")
-  system_policy_names    = ["AWSGlueDataBrewServiceRole"]
-  custom_policy_names    = [
-    "jacky_AwsGlueDataBrewServicePolicyForOutputS3-ID", "jacky_AwsGlueDataBrewServicePolicyForInputS3-ID", "AwsGlueDataBrewServicePolicyGlueCatalog"
+  source              = "../../cedc_terraform_generic_modules/modules/iam"
+  role_name           = "jacky_databrew_common_role"
+  trust_policy        = file("${path.module}/trust_policy.json")
+  system_policy_names = ["AWSGlueDataBrewServiceRole"]
+  custom_policy_names = [
+    "jacky_AwsGlueDataBrewServicePolicyForOutputS3-ID",
+    "jacky_AwsGlueDataBrewServicePolicyForInputS3-ID",
+    "AwsGlueDataBrewServicePolicyGlueCatalog"
   ]
   custom_policy_contents = [
     file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3.json"),
-    file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3.json", file("${path.module}/AwsGlueDataBrewServicePolicyGlueCatalog.json")
-      ]
-      }
+    file("${path.module}/AwsGlueDataBrewServicePolicyForOutputS3.json"),
+    file("${path.module}/AwsGlueDataBrewServicePolicyGlueCatalog.json")
+  ]
+}
 
 
 
