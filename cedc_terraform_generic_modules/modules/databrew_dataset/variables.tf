24103f6f0f02ec5769a9e9ea5c08f593965a2ca5
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-22 12:11:58 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/databrew_dataset/variables.tf
+++ cedc_terraform_generic_modules/modules/databrew_dataset/variables.tf
@@ -18,3 +18,25 @@ variable "dependencies" {
   type    = list(string)
   default = []
 }
+
+variable "bucket" {
+  description = "the name of s3 bucket"
+  type        = string
+}
+
+variable "key" {
+  description = "the name of s3 bucket folder"
+  type        = string
+}
+
+variable "delimiter" {
+  description = "csv format"
+  type        = string
+  default     = "Comma(,)"
+}
+
+variable "header_row" {
+  description = "csv format header row"
+  type        = string
+  default     = true
+}
