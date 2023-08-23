9a22aad8fbe45473ebe92027ed4f098172a1bd98
wangyouqun <530192791@QQ.com>
2023-08-23 16:54:39 +0800

    test dataset build

--- cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
+++ cedc_terraform_development/cedc_databrew_iam_common/cedc_databrew_iam_role.tf
@@ -1,11 +1,11 @@
 module "cedc_databrew_common_role" {
   source              = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name           = "jacky_databrew_common_role"
+  role_name           = "youqun_databrew_common_role"
   trust_policy        = file("${path.module}/trust_policy.json")
   system_policy_names = ["AWSGlueDataBrewServiceRole"]
   custom_policy_names = [
-    "jacky_AwsGlueDataBrewServicePolicyForOutputS3-ID",
-    "jacky_AwsGlueDataBrewServicePolicyForInputS3-ID",
+    "youqun_AwsGlueDataBrewServicePolicyForOutputS3-ID",
+    "youqun_AwsGlueDataBrewServicePolicyForInputS3-ID",
     "AwsGlueDataBrewServicePolicyGlueCatalog"
   ]
   custom_policy_contents = [
--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,12 +1,12 @@
 module "databrew_profile_job" {
   source       = "../../../cedc_terraform_generic_modules/modules/databrew_profile_job"
-  name         = "Jacky Customer profile job"
-  role_name    = "jacky_databrew_common_role"
-  dataset_name = "jacky-customers"
+  name         = "youqun Customer profile job"
+  role_name    = "youqun_databrew_common_role"
+  dataset_name = "youqun-customers"
   mode         = "FULL_DATASET"
   bucket       = "scriptbucket"
   bucket_owner = "ETL_Account"
-  key          = "jacky_databrew_workshop/profile-output/"
+  key          = "youqun_databrew_workshop/profile-output/"
   entity_types = ["USA_ALL"]
   dependencies = [
     "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
