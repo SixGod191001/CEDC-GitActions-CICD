0b961cbe2047ea59d7abb569a65173be95905995
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-23 14:53:56 +0800

    Update databrew_dataset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -2,5 +2,5 @@ module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "jyuwei-customers"
   database_name  = "testdb"
-  table_name     = "customers"
+  table_name     = "yuwei_databrew_workshop/datafiles/customers/customers.csv"
 }
