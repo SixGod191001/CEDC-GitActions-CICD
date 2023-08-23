404db16e0975a4e9440c20a1af051d8bab1a9350
wangyouqun <530192791@QQ.com>
2023-08-23 13:49:21 +0800

    test dataset build

--- cedc_terraform_development/youqun_databrew/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/youqun_databrew/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,6 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "haohao-customers"
+  name           = "youqun-customers"
   bucket         = "gitaction-s3-terraform"
-  key            = "haohao_databrew_workshop/datafiles/customers/customers.csv"
+  key            = "youqun_databrew/datafiles/customers/customers.csv"
 }
