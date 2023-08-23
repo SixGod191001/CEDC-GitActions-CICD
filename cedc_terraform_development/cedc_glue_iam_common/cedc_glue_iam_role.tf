8b3590f466bc4880070ceddacaf124fc5c786854
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-23 14:11:22 +0800

    Update cedc_glue_iam_role.tf

--- cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
+++ cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
@@ -1,9 +1,9 @@
 module "step_functions_iam_role" {
   source                 = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name              = "jacky_glue_execute_role"
+  role_name              = "yuwei_glue_execute_role"
   trust_policy           = file("${path.module}/trust_policy.json")
   system_policy_names    = ["CloudSearchFullAccess"]
-  custom_policy_names    = ["jacky_glue_invoke_s3_policy","jacky_s3_full_policy","jacky_glue_catalog_access","jacky_glue_service_policy"]
+  custom_policy_names    = ["yuwei_glue_invoke_s3_policy","yuwei_s3_full_policy","yuwei_glue_catalog_access","yuwei_glue_service_policy"]
   custom_policy_contents = [file("${path.module}/glue_invoke_s3_policy.json"),
                     file("${path.module}/s3_full_policy.json"),
                     file("${path.module}/s3_full_policy.json"),
