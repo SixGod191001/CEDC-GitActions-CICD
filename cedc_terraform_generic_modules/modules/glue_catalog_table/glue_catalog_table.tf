4dff9ada328f11226e968127bf64f110ce0195a6
elephantjr <elephant.exe@qq.com>
2023-08-23 16:00:30 +0800

    Update glue_catalog_table.tf

--- cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
+++ cedc_terraform_generic_modules/modules/glue_catalog_table/glue_catalog_table.tf
@@ -1,22 +1,26 @@
 locals {
-  table_list        = fileset(var.table_strctr_file_path, var.glue_catalog_table_list)
+     table_list     = fileset(var.table_strctr_file_path, var.glue_catalog_table_list)
 } 
+
+data "aws_ssm_parameter" "bucket" {
+           name     = var.datafile_bucket_name
+}
 
 resource "aws_glue_catalog_table" "create_catalog_table" {
 
-  for_each          = { for file in local.table_list : file => regex("[a-zA-Z\\-0-9_]+",file) }
+       for_each     = { for file in local.table_list : file => regex("[a-zA-Z\\-0-9_]+",file) }
   database_name     = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).database                                                                       #aws_glue_catalog_database.create_catalog_database.name
-  name              = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name                                                                     #local.table_structure.table_name
+           name     = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name                                                                     #local.table_structure.table_name
 
   #Storage Descriptor: Specify table's storage format and location
   storage_descriptor {
-    input_format    = "org.apache.hadoop.mapred.TextInputFormat"
-    output_format   = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
-    location        = "s3://${var.datafile_bucket_name}/${var.s3_key}/${jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name}.csv"
+   input_format     = "org.apache.hadoop.mapred.TextInputFormat"
+  output_format     = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+       location     = "s3://${data.aws_ssm_parameter.bucket.value}/${var.s3_key}/${jsondecode(file("${var.table_strctr_file_path}/${each.key}")).table_name}/"
 
     dynamic "columns" {
-      iterator      = columns
-      for_each      = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).columns                                                                        #jsondecode(data.local_file.table_schema.content).columns               #local.table_structure.columns
+       iterator     = columns
+       for_each     = jsondecode(file("${var.table_strctr_file_path}/${each.key}")).columns                                                                        #jsondecode(data.local_file.table_schema.content).columns               #local.table_structure.columns
 
         content {
           name      = columns.value.name
@@ -25,7 +29,7 @@ resource "aws_glue_catalog_table" "create_catalog_table" {
     }
 
     ser_de_info {
-      name                  = "SerDe"
+          name      = "SerDe"
       serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
       parameters = {
         "field.delim" = ","
