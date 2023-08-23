7f7d24d351652e4f7bc0b4d031c73d3d3138bda7
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-23 14:36:51 +0800

    Update iam.tf

--- cedc_terraform_generic_modules/modules/iam/iam.tf
+++ cedc_terraform_generic_modules/modules/iam/iam.tf
@@ -20,7 +20,5 @@ resource "aws_iam_policy_attachment" "iam_system_policy_attachment" {
   name       = "system-policy-attachment-${var.system_policy_names[count.index]}"
   count      = length(var.system_policy_names)
   roles      = [aws_iam_role.iam_role.name]
-  policy_arn = "var.use_service_role_arn ? "arn:aws:iam::aws:policy/service-role/${var.system_policy_names[count.index]}" : "arn:aws:iam::aws:policy/${var.system_policy_names[count.index]}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/${var.system_policy_names[count.index]}"
 }
-
-
