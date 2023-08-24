8cc1ee4d7b40b93f450c6aeb5a27381b26c8fdd1
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-08-24 22:54:30 +0800

    Update cedc_glue_iam_role.tf

--- cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
+++ cedc_terraform_development/cedc_glue_iam_common/cedc_glue_iam_role.tf
@@ -1,6 +1,6 @@
 module "step_functions_iam_role" {
   source                 = "../../cedc_terraform_generic_modules/modules/iam"
-  role_name              = "macui_glue_execute_role"
+  role_name              = "macui_glue_execute_role1"
   trust_policy           = file("${path.module}/trust_policy.json")
   system_policy_names    = ["CloudSearchFullAccess"]
   custom_policy_names    = ["macui_glue_invoke_s3_policy","macui_s3_full_policy","macui_glue_catalog_access","macui_glue_service_policy"]
