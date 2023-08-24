5218f3d687b45d9be25f08f08dd18bb792e6949a
YANG YANG <SixGod2019@outlook.com>
2023-08-24 22:20:48 +0800

    add sales dataset and delete customer job

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,13 @@
-module "databrew_dataset" {
+module "databrew_dataset_customers" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "jacky-customers"
   database_name  = "testdb"
   table_name     = "customers"
+}
+
+module "databrew_dataset_sales" {
+  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+  name           = "jacky-sales"
+  database_name  = "testdb"
+  table_name     = "sales"
 }
--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,46 +1,46 @@
-module "databrew_profile_job" {
-  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_profile_job"
-  name                             = "jacky Customer profile job"
-  role_name                        = "jacky_databrew_common_role"
-  type                             = "PROFILE"
-  dataset_name                     = "jacky-customers"
-  mode                             = "FULL_DATASET"
-  bucket                           = "scriptbucket"
-  bucket_owner                     = "ETL_Account"
-  key                              = "jacky_databrew_workshop/profile-output/"
-  entity_types                     = ["USA_ALL"]
-  dataset_statistics_configuration = {
-    included_statistics = ["CORRELATION"]
-    overrides           = [
-      {
-        parameters = {
-          "columnSelectors" : "[{\"name\":\"first_name\"}, {\"regex\":\"middle.*\"}]"
-        }
-        statistic = "CORRELATION"
-      }
-    ]
-  }
-  #  column_statistics_configurations = [
-  #    {
-  #      selectors : [
-  #        { name : "name" },
-  #        { regex : "middle*" }
-  #      ],
-  #      statistics : {
-  #        included_statistics : ["CORRELATION", "DUPLICATE_ROWS_COUNT"]
-  #        overrides : [
-  #          {
-  #            statistic : "VALUE_DISTRIBUTION",
-  #            parameters : {
-  #              "binNumber" : "10"
-  #            }
-  #          }
-  #        ]
-  #      }
-  #    }
-  #  ]
-
-  dependencies = [
-    "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
-  ]
-}
+#module "databrew_profile_job" {
+#  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_job"
+#  name                             = "jacky Customer profile job"
+#  role_name                        = "jacky_databrew_common_role"
+#  type                             = "PROFILE"
+#  dataset_name                     = "jacky-customers"
+#  mode                             = "FULL_DATASET"
+#  bucket                           = "scriptbucket"
+#  bucket_owner                     = "ETL_Account"
+#  key                              = "jacky_databrew_workshop/profile-output/"
+#  entity_types                     = ["USA_ALL"]
+#  dataset_statistics_configuration = {
+#    included_statistics = ["CORRELATION"]
+#    overrides           = [
+#      {
+#        parameters = {
+#          "columnSelectors" : "[{\"name\":\"first_name\"}, {\"regex\":\"middle.*\"}]"
+#        }
+#        statistic = "CORRELATION"
+#      }
+#    ]
+#  }
+#  #  column_statistics_configurations = [
+#  #    {
+#  #      selectors : [
+#  #        { name : "name" },
+#  #        { regex : "middle*" }
+#  #      ],
+#  #      statistics : {
+#  #        included_statistics : ["CORRELATION", "DUPLICATE_ROWS_COUNT"]
+#  #        overrides : [
+#  #          {
+#  #            statistic : "VALUE_DISTRIBUTION",
+#  #            parameters : {
+#  #              "binNumber" : "10"
+#  #            }
+#  #          }
+#  #        ]
+#  #      }
+#  #    }
+#  #  ]
+#
+#  dependencies = [
+#    "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
+#  ]
+#}
