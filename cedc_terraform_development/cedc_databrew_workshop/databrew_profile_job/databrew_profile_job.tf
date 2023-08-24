4ef0ea6585013f579e2a8a041e18ffa69a9d1177
YANG YANG <124654817+SixGod191001@users.noreply.github.com>
2023-08-24 22:47:44 +0800

    Update databrew_profile_job.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,13 +1,13 @@
 module "databrew_profile_job" {
   source       = "../../../cedc_terraform_generic_modules/modules/databrew_profile_job"
-  name         = "Jacky Customer profile job"
-  role_name    = "jacky_databrew_common_role"
+  name         = "Macui Customer profile job"
+  role_name    = "macui_databrew_common_role"
   type         = "PROFILE"
-  dataset_name = "jacky-customers"
+  dataset_name = "Macui-customers"
   mode         = "FULL_DATASET"
   bucket       = "scriptbucket"
   bucket_owner = "ETL_Account"
-  key          = "jacky_databrew_workshop/profile-output/"
+  key          = "macui_databrew_workshop/profile-output/"
   dependencies = [
     "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
   ]
