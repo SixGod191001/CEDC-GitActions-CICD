744115e74316d2661260a691a3901c472de9a1e6
YANG YANG <SixGod2019@outlook.com>
2023-08-25 22:00:09 +0800

    重新测试所有

--- cedc_terraform_generic_modules/modules/iam/iam.tf
+++ cedc_terraform_generic_modules/modules/iam/iam.tf
@@ -28,5 +28,10 @@ data "aws_iam_policy" "system_policy" {
   name     = each.key
 }
 
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 15"
 
+  depends_on = [aws_iam_role.iam_role]
+}
 
