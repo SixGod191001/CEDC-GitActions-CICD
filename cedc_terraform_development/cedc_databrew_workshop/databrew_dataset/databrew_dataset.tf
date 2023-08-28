dd5cca6261af40fa35e77f9616734b35f1712cbd
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-28 15:52:04 +0800

    Update databrew_dataset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,6 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "liuyuwei-dataset"
+  name           = "yuwei-dataset"
   bucket         = "gitaction-s3-terraform"
   key            = "cedc_databrew_workshop/datafiles/"
 }
