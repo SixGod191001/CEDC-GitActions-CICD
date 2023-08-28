57a3f5a54d57e0ce9a6a3db35ec76219ec59ac17
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-28 16:23:10 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/databrew_dataset/variables.tf
+++ cedc_terraform_generic_modules/modules/databrew_dataset/variables.tf
@@ -1,3 +1,8 @@
+variable "name" {
+  description = "The dataset name must contain 1-255 characters. Valid characters are alphanumeric (A-Z, a-z, 0-9), hyphen (-), period (.), and space."
+  type        = string
+}
+
 #variable "database_name" {
 #  description = "Glue catalog database name"
 #  type        = string
