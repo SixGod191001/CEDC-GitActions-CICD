92d01228fbc812c0367db95d9a03b168202e7bd7
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-28 13:54:42 +0800

    Update dataset.tf
    create dataset modlue

--- cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
+++ cedc_terraform_generic_modules/modules/databrew_dataset/dataset.tf
@@ -1,9 +1,15 @@
 resource "awscc_databrew_dataset" "dataset" {
   name  = var.name
   input = {
-    data_catalog_input_definition = {
-      database_name    = var.database_name
-      table_name       = var.table_name
+    s3_input_definition = {
+      bucket    = var.bucket
+      key       = var.key
+    }
+  }
+  format_options = {
+    csv = {
+      delimiter   = var.delimiter
+      header_row  = var.header_row
     }
   }
 }
