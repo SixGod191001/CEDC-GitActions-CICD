ece4ab6fb41d7ccea0106d57ba4d6da8695d0a3b
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-29 13:48:24 +0800

    Update databrew_dataset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -4,3 +4,12 @@ module "databrew_dataset" {
   bucket         = "gitaction-s3-terraform"
   key            = "haohao_databrew_workshop/datafiles/customers/"
 }
+
+module "databrew_dataset_sales" {
+  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+  name           = "haohao-sales"
+  bucket         = "gitaction-s3-terraform"
+  key            = "haohao_databrew_workshop/datafiles/customers/sales/"
+  depends_on     = [module.databrew_dataset]
+}
+
