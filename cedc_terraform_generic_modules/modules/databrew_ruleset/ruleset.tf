0437c32efd358fcbd965a1595a45ad52d77d4d00
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-29 16:49:33 +0800

    Create ruleset.tf

---
+++ cedc_terraform_generic_modules/modules/databrew_ruleset/ruleset.tf
@@ -0,0 +1,33 @@
+resource "awscc_databrew_ruleset" "ruleset" {
+  description = var.ruleset_description
+  name        = var.ruleset_name
+  target_arn  = local.target_arn
+
+  rules = {
+    check_expression = var.check_expression
+    name             = var.name
+    disabled         = var.disabled
+    column_selectors = {
+      name  = var.name
+      regex = var.regex
+    }
+    
+    threshold = {
+      value = var.value
+      type  = var.type
+      unit  = var.unit
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_ssm_parameter" "account_number" {
+  count = var.ssm_name_for_account_number != null ? 1 : 0
+  name  = var.ssm_name_for_account_number
+}
+
+locals {
+  account_number = var.ssm_name_for_account_number != null ? data.aws_ssm_parameter.account_number[0].value : data.aws_caller_identity.current.account_id
+  target_arn     = "arn:aws:databrew:${var.region}:${local.account_number}:dataset/${var.dataset_name}"
+}
