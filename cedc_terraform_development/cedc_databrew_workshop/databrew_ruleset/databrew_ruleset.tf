d1ed25974b5535a6fa8095b6494b473e010970be
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-29 17:03:54 +0800

    Create databrew_ruleset.tf

---
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
@@ -0,0 +1,30 @@
+module "example_ruleset" {
+  source = "../../../cedc_terraform_generic_modules/modules/databrew_ruleset"
+
+  ruleset_description = "Sales rule set"
+  ruleset_name        = "Haohao Sales DQ Checks"
+
+  rules               = [
+    {
+      check_expression = "total_sales > 0" # Data quality check
+      column_selectors = [
+        {
+          name  = "total_sales"
+          regex = null
+        }
+      ]
+      disabled         = false
+      name             = "SalesCheck"
+      substitution_map = null
+      threshold = {
+        type  = "GREATER_THAN" # threshold.type value must be one of: ["GREATER_THAN_OR_EQUAL" "LESS_THAN_OR_EQUAL" "GREATER_THAN" "LESS_THAN"]
+        unit  = "PERCENTAGE" # threshold.unit value must be one of: ["COUNT""PERCENTAGE"]
+        value = 90.0
+      }
+    }
+  ]
+
+  region       = "ap-northeast-1"
+  dataset_name = "haohao-sales"
+  ]
+}
