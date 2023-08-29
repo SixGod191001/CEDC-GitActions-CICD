47b97c481acd4e451514f4f9e24a4a71e89def41
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-08-29 15:05:00 +0800

    Update s3_objects.tf

--- cedc_terraform_development/cedc_databrew_workshop/s3_objects/s3_objects.tf
+++ cedc_terraform_development/cedc_databrew_workshop/s3_objects/s3_objects.tf
@@ -1,7 +1,7 @@
 module "prepared_s3_objects" {
   source         = "../../../cedc_terraform_generic_modules/modules/s3_object"
   s3_bucket_name = "scriptbucket"
-  keys           = ["haohao_databrew_workshop"]
+  keys           = ["kunyuan_databrew_workshop"]
   files          = ["${path.module}/datafiles/","${path.module}/feature-engineering/","${path.module}/profile-output/","${path.module}/recipes/","${path.module}/shared/"]
 }
 
