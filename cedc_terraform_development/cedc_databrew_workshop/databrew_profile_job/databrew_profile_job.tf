fc491e0652f84cc8067049aecbc70db98a9a9a4e
wangyouqun <530192791@QQ.com>
2023-08-29 16:44:41 +0800

    update rules

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -8,19 +8,5 @@ module "databrew_profile_job" {
   bucket                           = "scriptbucket"
   bucket_owner                     = "ETL_Account"
   key                              = "youqun_databrew_workshop/profile-output/"
-  entity_types                     = ["USA_ALL"]
-  dataset_statistics_configuration = {
-    included_statistics = ["CORRELATION"]
-    overrides           = [
-      {
-        parameters = {
-          "columnSelectors" : "[{\"name\":\"first_name\"}, {\"regex\":\"middle.*\"}]"
-        }
-        statistic = "CORRELATION"
-      }
-    ]
-  }
-  dependencies = [
-    "cedc_terraform_development/cedc_databrew_iam_common", "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
-  ]
+  entity_types                     = ["PERSON_NAME","DATE"]
 }
---
+++ cedc_terraform_generic_modules/modules/databrew_ruleset/ruleset.tf
@@ -0,0 +1,19 @@
+resource "awscc_databrew_ruleset" "ruleset" {
+  description = var.ruleset_description
+  name        = var.ruleset_name
+  rules       = var.rules
+  tags        = var.tags
+  target_arn  = local.target_arn
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_ssm_parameter" "account_number" {
+  count = var.ssm_name_for_account_number != null ? 1 : 0
+  name = var.ssm_name_for_account_number
+}
+
+locals {
+  account_number = var.ssm_name_for_account_number != null ? data.aws_ssm_parameter.account_number[0].value : data.aws_caller_identity.current.account_id
+  target_arn     = "arn:aws:databrew:${var.region}:${local.account_number}:dataset/${var.dataset_name}"
+}
