15869fbed789f3d761c9e313c6019f601cdca912
Cherry <643175794@qq.com>
2023-08-29 16:10:20 +0800

    add dataset_sales

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -4,3 +4,10 @@ module "databrew_dataset" {
   bucket         = "gitaction-s3-terraform"
   key            = "cedc_databrew_workshop/datafiles/customers/customers.csv"
 }
+
+module "databrew_dataset_sales" {
+  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+  name           = "elaine-sales"
+  bucket         = "gitaction-s3-terraform"
+  key            = "cedc_databrew_workshop/datafiles/customers/sales/"
+}
