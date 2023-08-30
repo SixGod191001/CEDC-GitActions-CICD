597b3d8f9663d58264a0d95367d11ee4ccae0b37
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-30 17:14:22 +0800

    Update databrew_dataset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,14 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "yuwei-dataset"
+  name           = "yuwei-customers"
   bucket         = "gitaction-s3-terraform"
-  key            = "yuwei_databrew_workshop/datafiles/"
+  key            = "yuwei_databrew_workshop/datafiles/customer.csv"
+}
+
+module "databrew_dataset_sales" {
+  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+  name           = "yuwei-sales"
+  bucket         = "gitaction-s3-terraform"
+  key            = "yuwei_databrew_workshop/datafiles/customers/sales/"
+  depends_on     = [module.databrew_dataset]
 }
