1d080329205cf4a985894b1281328ee34e1a2d3f
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-30 17:25:50 +0800

    Update databrew_profile_job.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,1 +1,23 @@
-
+module "databrew_profile_job" {
+  source                           = "../../../yuwei_terraform_generic_modules/modules/databrew_job"
+  name                             = "yuwei customer profile job"
+  role_name                        = "yuwei_databrew_common_role"
+  type                             = "PROFILE"
+  dataset_name                     = "yuwei-customers"
+  mode                             = "FULL_DATASET"
+  bucket                           = "scriptbucket"
+  bucket_owner                     = "ETL_Account"
+  key                              = "yuwei_databrew_workshop/profile-output/"
+  entity_types                     = ["USA_ALL"]
+  dataset_statistics_configuration = {
+    included_statistics = ["CORRELATION"]
+    overrides           = [
+      {
+        parameters = {
+          "columnSelectors" : "[{\"name\":\"first_name\"}, {\"regex\":\"middle.*\"}]"
+        }
+        statistic = "CORRELATION"
+      }
+    ]
+  }
+}
