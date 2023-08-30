eeb85bf8ce975bf128ea11e5f86895f0d4bb54d2
wangyouqun <530192791@QQ.com>
2023-08-30 17:49:12 +0800

    update job

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -9,6 +9,6 @@ module "databrew_dataset_sales" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "youqun-sales"
   bucket         = "gitaction-s3-terraform"
-  key            = "cedc_databrew_workshop/datafiles/customers/sales/"
+  key            = "cedc_databrew_workshop/datafiles/sales/sales.csv"
   depends_on     = [module.databrew_dataset]
 }
