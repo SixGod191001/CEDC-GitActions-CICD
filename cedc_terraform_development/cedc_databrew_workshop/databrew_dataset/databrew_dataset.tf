758879fc246a545a6aa05a5dc65a812808582764
liuzhu <62790503+Untitled1988@users.noreply.github.com>
2023-08-30 06:08:35 +0000

    update datasets

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,10 +1,10 @@
-module "databrew_dataset" {
+module "databrew_dataset_customers" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "liuzhu-customers"
   bucket         = "gitaction-s3-terraform"
   key            = "liuzhu_databrew_workshop/datafiles/customers/customers.csv"
 }
-module "databrew_dataset" {
+module "databrew_dataset_sales" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "liuzhu-sales"
   bucket         = "gitaction-s3-terraform"
