bb045202614ebd704edba41a1ac3a3a6cb3ce711
Cherry <643175794@qq.com>
2023-08-31 13:27:33 +0800

    update job

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -2,12 +2,12 @@ module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "elaine-customers"
   bucket         = "gitaction-s3-terraform"
-  key            = "cedc_databrew_workshop/datafiles/customers/customers.csv"
+  key            = "elaine_cedc_databrew_workshop/datafiles/customers/customers.csv"
 }
 
 module "databrew_dataset_sales" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
   name           = "elaine-sales"
   bucket         = "gitaction-s3-terraform"
-  key            = "cedc_databrew_workshop/datafiles/customers/sales/"
+  key            = "elaine_cedc_databrew_workshop/datafiles/sales/sales.csv"
 }
--- cedc_terraform_development/cedc_databrew_workshop/s3_objects/s3_objects.tf
+++ cedc_terraform_development/cedc_databrew_workshop/s3_objects/s3_objects.tf
@@ -1,7 +1,7 @@
 module "prepared_s3_objects" {
   source         = "../../../cedc_terraform_generic_modules/modules/s3_object"
   s3_bucket_name = "scriptbucket"
-  keys           = ["cedc_databrew_workshop"]
+  keys           = ["elaine_cedc_databrew_workshop"]
   files          = ["${path.module}/datafiles/","${path.module}/feature-engineering/","${path.module}/profile-output/","${path.module}/recipes/","${path.module}/shared/"]
 }
 
