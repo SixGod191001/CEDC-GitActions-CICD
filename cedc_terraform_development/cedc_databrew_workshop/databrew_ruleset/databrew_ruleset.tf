83c4a890ce7ef1d69f57faee209e7534e1319205
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-08-31 14:38:08 +0800

    Update ruleset

--- cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
@@ -7,7 +7,8 @@ module "example_ruleset" {
   rules               = [
     {
       # check_expression = "AGG(DUPLICATE_VALUES_COUNT) <:val" # Data quality check  duplicate rows count == 0
-      check_expression = "AGG(MISSING_VALUES_PERCENTAGE) == :val1"
+      # check_expression = "AGG(MISSING_VALUES_PERCENTAGE) == :val1"
+      check_expression = "total_sales > val1"
       column_selectors = null
       disabled         = false
       name             = "Check missing value"
