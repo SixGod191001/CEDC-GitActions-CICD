bfe0deb79d509c871baa4eb92ed207e06db5a0d7
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-31 10:03:09 +0800

    Update databrew_profile_job.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,4 +1,4 @@
-/*
+
 module "databrew_profile_job" {
   source                           = "../../../cedc_terraform_generic_modules/modules/databrew_job"
   name                             = "yuwei customer profile job"
@@ -25,4 +25,3 @@ module "databrew_profile_job" {
    "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
   ]
 }
-*/
