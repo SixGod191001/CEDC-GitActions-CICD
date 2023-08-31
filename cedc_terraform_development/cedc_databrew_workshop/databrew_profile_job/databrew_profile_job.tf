37243ab94490bcd1d3b68ac74bb7d6f2b482c4a7
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-08-31 09:46:15 +0800

    Update databrew_profile_job.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -7,21 +7,20 @@ module "databrew_profile_job" {
   mode                             = "FULL_DATASET"
   bucket                           = "scriptbucket"
   bucket_owner                     = "ETL_Account"
-  key                              = "yuwei_databrew_workshop/profile-output/"
+  key                              = "liuyuwei_databrew_workshop/profile-output/"
   entity_types                     = ["USA_ALL"]
   dataset_statistics_configuration = {
     included_statistics = ["CORRELATION"]
     overrides           = [
       {
         parameters = {
-          "columnSelectors" : "[{\"name\":\"first_name\"}, {\"regex\":\"middle.*\"}]"
+          "columnSelectors" : "[{\"name\":\"Customer_Id\"}, {\"regex\":\"Total.*\"}]"
         }
         statistic = "CORRELATION"
       }
     ]
   }
+  dependencies = [
+   "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
+  ]
 }
-depends_on = [
-    scriptbucket,
-    yuwei_databrew_common_role,
-]
