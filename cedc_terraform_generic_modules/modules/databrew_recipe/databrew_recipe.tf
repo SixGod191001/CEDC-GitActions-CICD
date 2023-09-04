f57a4d39828449b3bb68d76673211815b063c0af
wangyouqun <530192791@QQ.com>
2023-09-04 11:53:35 +0800

    update project

--- cedc_terraform_generic_modules/modules/databrew_recipe/databrew_recipe.tf
+++ cedc_terraform_generic_modules/modules/databrew_recipe/databrew_recipe.tf
@@ -1,4 +1,21 @@
 resource "aws_databrew_recipe" "example_recipe" {
   name         = var.recipe_name
-  recipe_steps = file("../../../cedc_terraform_development/cedc_databrew_workshop/databrew_project/recipe.json")
+  recipe_steps = <<EOF
+[
+  {
+    "action": "header",
+    "parameters": {
+      "datasetColumns": "Txn_Date,Customer_Id,Product_Id,Quantity,Total_Sales",
+      "headerRow": "true"
+    }
+  },
+  {
+    "action": "rename_column",
+    "parameters": {
+      "newColumnName": "Product_Id4",
+      "oldColumnName": "Product_Id"
+    }
+  }
+]
+EOF
 }
