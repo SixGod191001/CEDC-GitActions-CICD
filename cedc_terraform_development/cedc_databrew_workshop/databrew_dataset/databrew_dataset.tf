5a40e7a6f41a061176f53b1def77c1140126ea3a
Cherry <643175794@qq.com>
2023-09-04 14:03:43 +0800

    clear all

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,13 +1,13 @@
-module "databrew_dataset" {
-  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "elaine-customers"
-  bucket         = "gitaction-s3-terraform"
-  key            = "elaine_cedc_databrew_workshop/datafiles/customers/customers.csv"
-}
-
-module "databrew_dataset_sales" {
-  source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "elaine-sales"
-  bucket         = "gitaction-s3-terraform"
-  key            = "elaine_cedc_databrew_workshop/datafiles/sales/sales.csv"
-}
+# module "databrew_dataset" {
+#   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+#   name           = "elaine-customers"
+#   bucket         = "gitaction-s3-terraform"
+#   key            = "elaine_cedc_databrew_workshop/datafiles/customers/customers.csv"
+# }
+#
+# module "databrew_dataset_sales" {
+#   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
+#   name           = "elaine-sales"
+#   bucket         = "gitaction-s3-terraform"
+#   key            = "elaine_cedc_databrew_workshop/datafiles/sales/sales.csv"
+# }
