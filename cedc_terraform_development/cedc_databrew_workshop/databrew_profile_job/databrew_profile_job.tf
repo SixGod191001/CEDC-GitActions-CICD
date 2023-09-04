61091054956211ed01794cb4c880457f66452909
wangyouqun <530192791@QQ.com>
2023-09-04 09:47:07 +0800

    update job

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job/databrew_profile_job.tf
@@ -1,26 +1,26 @@
-#module "databrew_profile_job" {
-#  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_job"
-#  name                             = "youqun sales profile job"
-#  role_name                        = "youqun_databrew_common_role"
-#  type                             = "PROFILE"
-#  dataset_name                     = "youqun-sales"
-#  mode                             = "FULL_DATASET"
-#  bucket                           = "scriptbucket"
-#  bucket_owner                     = "ETL_Account"
-#  key                              = "youqun_databrew_workshop/profile-output/"
-#  entity_types                     = ["USA_ALL"]
-#  dataset_statistics_configuration = {
-#    included_statistics = ["CORRELATION"]
-#    overrides           = [
-#      {
-#        parameters = {
-#          "columnSelectors" : "[{\"name\":\"Customer_Id\"}, {\"regex\":\"Total.*\"}]"
-#        }
-#        statistic = "CORRELATION"
-#      }
-#    ]
-#  }
-#  dependencies = [
-#   "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
-#  ]
-#}
+module "databrew_profile_job" {
+  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_job"
+  name                             = "youqun customer profile job"
+  role_name                        = "youqun_databrew_common_role"
+  type                             = "PROFILE"
+  dataset_name                     = "youqun-customers"
+  mode                             = "FULL_DATASET"
+  bucket                           = "scriptbucket"
+  bucket_owner                     = "ETL_Account"
+  key                              = "youqun_databrew_workshop/profile-output/"
+  entity_types                     = ["USA_ALL"]
+  dataset_statistics_configuration = {
+    included_statistics = ["CORRELATION"]
+    overrides           = [
+      {
+        parameters = {
+          "columnSelectors" : "[{\"name\":\"Customer_Id\"}, {\"regex\":\"Middle.*\"}]"
+        }
+        statistic = "CORRELATION"
+      }
+    ]
+  }
+  dependencies = [
+   "cedc_terraform_development/cedc_databrew_workshop/databrew_dataset"
+  ]
+}
