2abee180541514a89dd509025b7415e7fbfa4637
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-09-05 14:36:03 +0800

    Update databrew_dataset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_dataset/databrew_dataset.tf
@@ -1,6 +1,6 @@
 module "databrew_dataset" {
   source         = "../../../cedc_terraform_generic_modules/modules/databrew_dataset"
-  name           = "kunyuan-datafiles"
+  name           = "kunyuan-customers"
   bucket         = "gitaction-s3-terraform"
-  key            = "kunyuan_databrew_workshop/datafiles/"
+  key            = "kunyuan_databrew_workshop/datafiles/customers"
 }
