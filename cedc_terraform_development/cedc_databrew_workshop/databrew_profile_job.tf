89c41681e594dc4179d3c5c9ff15226be0f3018d
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-09-05 22:07:21 +0800

    Update databrew_profile_job.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job.tf
@@ -1,5 +1,5 @@
 module "databrew_profile_job" {
-  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_profile_job"
+  source                           = "../../cedc_terraform_generic_modules/modules/databrew_profile_job"
   name                             = "macui customer profile job"
   role_name                        = "macui_databrew_common_role"
   type                             = "PROFILE"
