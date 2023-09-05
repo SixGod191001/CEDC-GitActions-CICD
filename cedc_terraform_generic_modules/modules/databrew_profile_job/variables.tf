3f7fdaeea786b9678f4d511cd2e4bfc54fad0a4f
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-09-05 21:34:10 +0800

    Update variables.tf

--- cedc_terraform_generic_modules/modules/databrew_profile_job/variables.tf
+++ cedc_terraform_generic_modules/modules/databrew_profile_job/variables.tf
@@ -1,21 +1,23 @@
 variable "name" {
-  description = "Job name"
+  description = "Job name (required)"
   type        = string
 }
 
 variable "role_name" {
-  description = "Role name"
+  description = "Role name (required)"
   type        = string
 }
 
 variable "type" {
   description = "Job type"
   type        = string
+  default     = null
   #  allowed_values = ["PROFILE", "RECIPE"]
 }
 
+
 variable "mode" {
-  description = "Sample configuration mode for profile jobs"
+  description = "Sample configuration mode for profile jobs (FULL_DATASET | CUSTOM_ROWS)"
   type        = string
   #  allowed_values = ["FULL_DATASET", "CUSTOM_ROWS"]
   default     = null
@@ -24,17 +26,17 @@ variable "mode" {
 variable "size" {
   description = "Sample configuration size for profile jobs"
   type        = number
-  default     = 20000
+  default     = null
 }
 
 variable "dataset_name" {
-  description = "Dataset name"
+  description = "Dataset name (required)"
   type        = string
   default     = null
 }
 
 variable "bucket" {
-  description = "S3 bucket name for output location, please input the right ssm parameters for this bucket"
+  description = "S3 bucket name for output location, please input the right ssm parameters for this bucket (required)"
   type        = string
 }
 
@@ -50,42 +52,13 @@ variable "key" {
   default     = null
 }
 
-variable "encryption_mode" {
-  description = "Encryption mode, SSE-KMS mode needs encryption_key_arn"
-  type        = string
-  #  allowed_values = ["SSE-KMS", "SSE-S3"]
-  default     = null
+variable "entity_types" {
+  description = "Dataset level configurations - PII categories (USA_ALL | USA_SSN | EMAIL | USA_ITIN | USA_PASSPORT_NUMBER | PHONE_NUMBER | USA_DRIVING_LICENSE | BANK_ACCOUNT | CREDIT_CARD | IP_ADDRESS | MAC_ADDRESS | USA_DEA_NUMBER | USA_HCPCS_CODE | USA_NATIONAL_PROVIDER_IDENTIFIER | USA_NATIONAL_DRUG_CODE | USA_HEALTH_INSURANCE_CLAIM_NUMBER | USA_MEDICARE_BENEFICIARY_IDENTIFIER | USA_CPT_CODE | PERSON_NAME | DATE)"
+  type        = list(string)
+  default     = []
+
 }
 
-variable "encryption_key_arn" {
-  description = "Encryption Key Arn"
-  type        = string
-  default     = null
-}
-
-variable "max_capacity" {
-  description = "Maximum number of units"
-  type        = number
-  default     = 5
-}
-
-variable "timeout" {
-  description = "Job timeout (minutes)"
-  type        = number
-  default     = 60
-}
-
-variable "max_reties" {
-  description = "Maximum number of times to retry job on failure"
-  type        = number
-  default     = 0
-}
-
-variable "tags" {
-  description = "tags"
-  type        = list(object({ key = string, value = string }))
-  default     = [{ key = "project", value = "CEDC" }]
-}
 
 variable "dependencies" {
   description = "Cross folder module dependencies"
