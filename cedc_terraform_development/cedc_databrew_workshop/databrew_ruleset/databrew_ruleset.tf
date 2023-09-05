1db73598e1ab85012829cece9f9367b310213dfd
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-09-05 15:32:59 +0800

    Update databrew_ruleset.tf

--- cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
@@ -2,18 +2,14 @@ module "example_ruleset" {
   source = "../../../cedc_terraform_generic_modules/modules/databrew_ruleset"
 
   ruleset_description = "Sales rule set"
-  ruleset_name        = "Haohao Sales DQ Checks"
+  ruleset_name        = "Kunyuan Sales DQ Checks"
 
   rules               = [
     {
-      # check_expression = "AGG(DUPLICATE_VALUES_COUNT) <:val" # Data quality check  duplicate rows count == 0
-      # check_expression = "AGG(MISSING_VALUES_PERCENTAGE) == :val1"
-      # check_expression = "`total_sales` > :val"
       check_expression = ":col > :val"
       column_selectors = null
       disabled         = false
       name             = "Total Sales should be >0"
-      # substitution_map = null
       substitution_map = [
         {
          value_reference = ":val"
@@ -29,5 +25,5 @@ module "example_ruleset" {
   ]
 
   region       = "ap-northeast-1"
-  dataset_name = "haohao-sales"
+  dataset_name = "kunyuan-sales"
 }
