64cd316a1452bb66cb48f13fc6b507a2c5bcc7f9
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-09-05 21:51:52 +0800

    Create databrew_profile_job.tf

---
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_profile_job.tf
@@ -0,0 +1,12 @@
+module "databrew_profile_job" {
+  source                           = "../../../cedc_terraform_generic_modules/modules/databrew_profile_job"
+  name                             = "macui customer profile job"
+  role_name                        = "macui_databrew_common_role"
+  type                             = "PROFILE"
+  dataset_name                     = "macui-customers"
+  mode                             = "FULL_DATASET"
+  bucket                           = "scriptbucket"
+  bucket_owner                     = "ETL_Account"
+  key                              = "macui_databrew_workshop/profile-output/"
+  entity_types                     = ["PERSON_NAME","DATE"]
+}
