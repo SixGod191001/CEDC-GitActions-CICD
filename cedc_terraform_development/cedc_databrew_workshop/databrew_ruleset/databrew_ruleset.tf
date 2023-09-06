04188e15b2a6dd46e6d959a513b143ab793e1782
Vickie <128558885+Vickie-ext@users.noreply.github.com>
2023-09-06 17:26:39 +0800

    Create databrew_ruleset.tf

---
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
@@ -0,0 +1,34 @@
+module "example_ruleset" {
+  source = "../../../cedc_terraform_generic_modules/modules/databrew_ruleset"
+
+  ruleset_description = "Sales ruleset"
+  ruleset_name        = "liuyuwei Sales Checks"
+
+  rules               = [
+    {
+      check_expression = ":col > :val"
+      column_selectors = null
+      disabled         = false
+      name             = "Total Sales should be >0"
+      substitution_map = [
+        {
+         value_reference = ":val"
+         value           = "0"
+        },
+        {
+         value_reference = ":col"
+         value           = "`Total_Sales`"
+        }
+       ]
+      threshold        = null
+    }
+  ]
+  tags = [
+    {
+      key   = "Project"
+      value = "CEDC"
+    }
+  ]
+  region       = "ap-northeast-1"
+  dataset_name = "liuyuwei-sales"
+}
