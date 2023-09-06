cb6227bbf16c782903e019238f2d595c21126e4d
macui <127483949+Nikkicat6@users.noreply.github.com>
2023-09-06 23:01:30 +0800

    Create databrew_ruleset.tf

---
+++ cedc_terraform_development/cedc_databrew_workshop/databrew_ruleset/databrew_ruleset.tf
@@ -0,0 +1,33 @@
+module "databrew_ruleset" {
+  source = "../../../cedc_terraform_generic_modules/modules/databrew_ruleset"
+
+  ruleset_description = "Sales rule set"
+  ruleset_name        = "Macui Sales DQ Checks"
+
+  rules               = [
+    {
+      # check_expression = "AGG(DUPLICATE_VALUES_COUNT) <:val" # Data quality check  duplicate rows count == 0
+      # check_expression = "AGG(MISSING_VALUES_PERCENTAGE) == :val1"
+      # check_expression = "`total_sales` > :val"
+      check_expression = ":col > :val"
+      column_selectors = null
+      disabled         = false
+      name             = "Total Sales should be >0"
+      # substitution_map = null
+      substitution_map = [
+        {
+         value_reference = ":val"
+         value           = "0"
+        },
+        {
+         value_reference = ":col"
+         value           = "`total_sales`"
+        }
+       ]
+      threshold        = null
+    }
+  ]
+
+  region       = "ap-northeast-1"
+  dataset_name = "macui-sales"
+}
