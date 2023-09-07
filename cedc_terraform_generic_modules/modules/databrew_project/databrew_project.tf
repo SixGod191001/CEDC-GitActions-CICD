2535e899d847717154fa94daf565e6d9cb860630
Han Kunyuan <127489804+HanKYOO@users.noreply.github.com>
2023-09-07 13:26:18 +0800

    Create databrew_project.tf

---
+++ cedc_terraform_generic_modules/modules/databrew_project/databrew_project.tf
@@ -0,0 +1,11 @@
+resource "awscc_databrew_project" "databrew_project"{
+  name               = var.name
+  dataset_name       = var.dataset_name
+  role_arn           = data.aws_iam_role.role_arn.arn
+  recipe_name        = var.recipe_name
+  }
+
+
+data "aws_iam_role" "role_arn" {
+  name = var.role_name
+}
